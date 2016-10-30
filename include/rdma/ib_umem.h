057ca7744b5760962c9d75dcdaaf57b4a2d1556b
Yishai Hadas <yishaih@mellanox.com>
2016-10-30 10:21:28 +0200

    IB/core: Infrastructure to manage peer core context
    Adds an infrastructure to manage core context for a given umem,
    it's needed for the invalidation flow.
    
    Core context is supplied to peer clients as some opaque data for a given
    memory pages represented by a umem.
    
    If the peer client needs to invalidate memory it provided through the
    peer memory callbacks,
    it should call the invalidation callback, supplying the relevant core
    context.
    IB core will use this context to invalidate the relevant memory.
    
    To prevent cases when there are inflight invalidation calls in parallel
    to releasing this memory (e.g. by dereg_mr) we must ensure that context
    is valid before accessing it, that's why couldn't use the core context
    pointer directly. For that reason we added a lookup table to map between
    a ticket id to a core context. Peer client will get/supply the ticket
    id, core will check whether exists before accessing its corresponding
    context.
    
    The ticket id is provided to the peer memory client, as part of the
    get_pages API. The only "remote" party using it is the peer memory
    client. It is used for invalidation flow, to specify what memory
    registration should be invalidated. This flow might be called
    asynchronously, in parallel to an ongoing dereg_mr operation. As such,
    the invalidation flow might be called after the memory registration
    has been completely released. Relying on a pointer-based, or IDR-based
    ticket value can result in spurious invalidation of unrelated memory
    regions. Internally, we carefully lock the data structures and
    synchronize as needed when extracting the context from the
    ticket. This ensures a proper, synchronized release of the memory
    mapping. The ticket mechanism allows us to safely ignore inflight
    invalidation calls that were arrived too late.
    
    issue: 862323
    Change-Id: I182c14258a7cf181987cd9ffc463705cab76859e
    Signed-off-by: Yishai Hadas <yishaih@mellanox.com>

--- include/rdma/ib_umem.h
+++ include/rdma/ib_umem.h
@@ -42,6 +42,11 @@
 struct ib_ucontext;
 struct ib_umem_odp;
 
+struct invalidation_ctx {
+	struct ib_umem *umem;
+	u64 context_ticket;
+};
+
 struct ib_umem {
 	struct ib_ucontext     *context;
 	struct mm_struct       *owning_mm;
@@ -56,6 +61,7 @@ struct ib_umem {
 	unsigned int    sg_nents;
 	/* peer memory that manages this umem */
 	struct ib_peer_memory_client *ib_peer_mem;
+	struct invalidation_ctx *invalidation_ctx;
 	/* peer memory private context */
 	void *peer_mem_client_context;
 };
