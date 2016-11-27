90a59e2555ccbccd9c1ad96ccea0cbc4a3687251
Max Gurtovoy <maxg@mellanox.com>
2016-11-27 14:11:37 +0200

    nvme: introduce nvme-peer header for future pci peering
    Issue: 920501
    
    Change-Id: Ic9927360f648f43dd12e759ee4cd53b9ae542932
    Signed-off-by: Max Gurtovoy <maxg@mellanox.com>

--- drivers/nvme/host/pci.c
+++ drivers/nvme/host/pci.c
@@ -18,6 +18,7 @@
 #include <linux/mutex.h>
 #include <linux/once.h>
 #include <linux/pci.h>
+#include <linux/nvme-peer.h>
 #include <linux/t10-pi.h>
 #include <linux/types.h>
 #include <linux/io-64-nonatomic-lo-hi.h>
@@ -92,6 +93,10 @@ struct nvme_dev;
 struct nvme_queue;
 
 static void nvme_dev_disable(struct nvme_dev *dev, bool shutdown);
+static int nvme_create_queue(struct nvme_queue *nvmeq, int qid, bool polled);
+static int nvme_suspend_queue(struct nvme_queue *nvmeq);
+static int adapter_delete_sq(struct nvme_dev *dev, u16 sqid);
+static int adapter_delete_cq(struct nvme_dev *dev, u16 cqid);
 static bool __nvme_disable_io_queues(struct nvme_dev *dev, u8 opcode);
 
 /*
@@ -215,6 +220,7 @@ struct nvme_queue {
 
 	/* p2p */
 	bool p2p;
+	struct nvme_peer_resource resource;
 };
 
 /*
@@ -235,6 +241,91 @@ struct nvme_iod {
 	dma_addr_t meta_dma;
 	struct scatterlist *sg;
 };
+
+static int nvme_peer_init_resource(struct nvme_queue *nvmeq,
+				   enum nvme_peer_resource_mask mask,
+				   void (* stop_master_peer)(struct pci_dev *pdev))
+{
+	struct nvme_dev *dev = nvmeq->dev;
+	int qid = nvmeq->qid;
+	int ret = 0;
+
+	if (mask & NVME_PEER_SQT_DBR)
+		/* Calculation from NVMe 1.2.1 SPEC */
+		nvmeq->resource.sqt_dbr_addr = dev->bar_phys_addr + (0x1000 + ((2 * (qid)) * (4 << NVME_CAP_STRIDE(lo_hi_readq(dev->bar + NVME_REG_CAP)))));
+
+	if (mask & NVME_PEER_CQH_DBR)
+		/* Calculation from NVMe 1.2.1 SPEC */
+		nvmeq->resource.cqh_dbr_addr = dev->bar_phys_addr + (0x1000 + ((2 * (qid) + 1) * (4 << NVME_CAP_STRIDE(lo_hi_readq(dev->bar + NVME_REG_CAP)))));
+
+	if (mask & NVME_PEER_SQ_PAS)
+		nvmeq->resource.sq_dma_addr = nvmeq->sq_dma_addr;
+
+	if (mask & NVME_PEER_CQ_PAS)
+		nvmeq->resource.cq_dma_addr = nvmeq->cq_dma_addr;
+
+	if (mask & NVME_PEER_SQ_SZ)
+		nvmeq->resource.nvme_sq_size = SQ_SIZE(dev->q_depth);
+
+	if (mask & NVME_PEER_CQ_SZ)
+		nvmeq->resource.nvme_cq_size = CQ_SIZE(dev->q_depth);
+
+	if (mask & NVME_PEER_MEM_LOG_PG_SZ)
+		nvmeq->resource.memory_log_page_size = __ffs(dev->ctrl.page_size >> 12); /* memory_log_page_size is in 4K granularity */
+
+	nvmeq->resource.flags = NVME_QUEUE_PHYS_CONTIG;
+	nvmeq->resource.stop_master_peer = stop_master_peer;
+
+	return ret;
+}
+
+void nvme_peer_put_resource(struct nvme_peer_resource *resource)
+{
+	struct nvme_queue *nvmeq = container_of(resource, struct nvme_queue,
+						resource);
+	mutex_lock(&resource->lock);
+	resource->in_use = false;
+	resource->stop_master_peer = NULL;
+
+	/* Restart the queue for future usage */
+	nvme_suspend_queue(nvmeq);
+	adapter_delete_sq(nvmeq->dev, nvmeq->qid);
+	adapter_delete_cq(nvmeq->dev, nvmeq->qid);
+
+	nvme_create_queue(nvmeq, nvmeq->qid, false);
+
+	mutex_unlock(&resource->lock);
+}
+EXPORT_SYMBOL_GPL(nvme_peer_put_resource);
+
+struct nvme_peer_resource *nvme_peer_get_resource(struct pci_dev *pdev,
+	enum nvme_peer_resource_mask mask,
+	void (* stop_master_peer)(struct pci_dev *pdev))
+{
+	struct nvme_dev *dev = pci_get_drvdata(pdev);
+	struct nvme_queue *nvmeq;
+	unsigned i;
+	int ret;
+
+	for (i = 0; i < dev->online_queues; i++) {
+		nvmeq = &dev->queues[i];
+		if (nvmeq->p2p) {
+			mutex_lock(&nvmeq->resource.lock);
+			if (!nvmeq->resource.in_use) {
+				ret = nvme_peer_init_resource(nvmeq, mask, stop_master_peer);
+				if (!ret) {
+					nvmeq->resource.in_use = true;
+					mutex_unlock(&nvmeq->resource.lock);
+					return &nvmeq->resource;
+				}
+			}
+			mutex_unlock(&nvmeq->resource.lock);
+		}
+	}
+
+	return NULL;
+}
+EXPORT_SYMBOL_GPL(nvme_peer_get_resource);
 
 static unsigned int max_io_queues(void)
 {
@@ -1508,6 +1599,8 @@ static int nvme_alloc_queue(struct nvme_dev *dev, int qid, int depth)
 	nvmeq->q_depth = depth;
 	nvmeq->qid = qid;
 	nvmeq->p2p = qid > (dev->max_qid - dev->num_p2p_queues);
+	if (nvmeq->p2p)
+		mutex_init(&nvmeq->resource.lock);
 	dev->ctrl.queue_count++;
 
 	return 0;
