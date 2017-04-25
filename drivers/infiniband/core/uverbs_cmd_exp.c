526df26dc3d566e4a823f60b0624a80e5f122272
Parav Pandit <parav@mellanox.com>
2017-04-25 16:45:22 -0500

    IB/uverbs: Define additional mask for out of order QP attribute
    - Copies ooo capabilities from IB core layer to uverb response.
    
    - Defines additional entry to the experimental QP attribute mask to
      consider while retriving from user space.
    
    issue: 969650
    Change-Id: I2f2511066c85fd9570d80cbf6c52ff9a2146b438
    Signed-off-by: Parav Pandit <parav@mellanox.com>
    Reviewed-by: Daniel Jurgens <danielj@mellanox.com>
    Reviewed-by: Eli Cohen <eli@mellanox.com>

--- drivers/infiniband/core/uverbs_cmd_exp.c
+++ drivers/infiniband/core/uverbs_cmd_exp.c
@@ -600,6 +600,13 @@ int ib_uverbs_exp_query_device(struct uverbs_attr_bundle *attrs)
 		resp->comp_mask |= IB_EXP_DEVICE_ATTR_PACKET_PACING_CAPS;
 	}
 
+	if (exp_attr->exp_comp_mask & IB_EXP_DEVICE_ATTR_OOO_CAPS) {
+		resp->ooo_caps.rc_caps = exp_attr->ooo_caps.rc_caps;
+		resp->ooo_caps.xrc_caps = exp_attr->ooo_caps.xrc_caps;
+		resp->ooo_caps.dc_caps = exp_attr->ooo_caps.dc_caps;
+		resp->comp_mask |= IB_EXP_DEVICE_ATTR_OOO_CAPS;
+	}
+
 	ret = ib_copy_to_udata( &attrs->ucore, resp, min_t(size_t, sizeof(*resp),  &attrs->ucore.outlen));
 out:
 	kfree(exp_attr);
