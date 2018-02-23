e9b677b6fa8da2ddb55581409dcf70595c9e950f
Huy Nguyen <huyn@mellanox.com>
2018-02-23 13:13:41 -0600

    IB/mlx5: fix capi rdma page fault error resume
    mlx5_ib_mr_rdma_pfault_handler expects the return value of
    pagefault_single_data_segment to be number of mapped pages.
    
    Issue: 1316706
    Change-Id: I291ccc367bba01d5c2e99af0c001a56b42cb6af9
    Signed-off-by: Huy Nguyen <huyn@mellanox.com>

--- drivers/infiniband/hw/mlx5/odp.c
+++ drivers/infiniband/hw/mlx5/odp.c
@@ -93,6 +93,7 @@ struct mlx5_pagefault {
 #define MLX5_KSM_PAGE_SHIFT MLX5_IMR_MTT_SHIFT
 
 static u64 mlx5_imr_ksm_entries;
+static int pages_in_range(u64 address, u32 length);
 
 static int check_parent(struct ib_umem_odp *odp,
 			       struct mlx5_ib_mr *parent)
@@ -700,8 +701,12 @@ next_mr:
 			return -1;
 		}
 		ret = handle_capi_pg_fault(dev, mr->umem->owning_mm, io_virt, bcnt);
-		if (!ret && bytes_mapped)
-			*bytes_mapped += bcnt;
+		if (!ret) {
+			if (bytes_mapped)
+				*bytes_mapped += bcnt;
+			return pages_in_range(io_virt, bcnt);
+		}
+
 		return ret;
 	}
 
