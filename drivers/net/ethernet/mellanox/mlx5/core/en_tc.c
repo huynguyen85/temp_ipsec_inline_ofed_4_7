8b0024a1eeb825b6c7ece7887df1b07a465979f7
Roi Dayan <roid@mellanox.com>
2019-03-17 11:45:14 +0200

    net/mlx5e: Add decap action for mirred egress rules
    Issue: 1730904
    Change-Id: Iece3cf281bf542d2ebcd80eac1ea050d9a9f8c79
    Signed-off-by: Roi Dayan <roid@mellanox.com>

--- drivers/net/ethernet/mellanox/mlx5/core/en_tc.c
+++ drivers/net/ethernet/mellanox/mlx5/core/en_tc.c
@@ -2855,6 +2855,13 @@ static int parse_tc_fdb_actions(struct mlx5e_priv *priv,
 
 				if (!mlx5e_eswitch_rep(out_dev))
 					return -EOPNOTSUPP;
+#ifdef CONFIG_MLX5_MINIFLOW
+
+				if (flow->flags & MLX5E_TC_FLOW_EGRESS) {
+					action |= MLX5_FLOW_CONTEXT_ACTION_DECAP;
+				}
+#endif
+
 
 				out_priv = netdev_priv(out_dev);
 				rpriv = out_priv->ppriv;
