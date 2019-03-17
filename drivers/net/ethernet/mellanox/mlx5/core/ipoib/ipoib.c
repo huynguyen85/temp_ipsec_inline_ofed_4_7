d10d6f1d850a1a4abd4ce17a5e2058cae46e7846
Feras Daoud <ferasda@mellanox.com>
2019-03-17 17:51:07 +0200

    net/mlx5: IPoIB, Fix mlx5e_get_netdev_max_channels for mlx5i profile
    mlx5e_get_netdev_max_channels used mlx5e priv to get the max
    number of channels for all netdev profiles. For enhanced IPoIB this is
    incorrect as mlx5i priv should be used.
    
    Issue: 1699311
    Fixes: 946582be0366 ("BACKPORTS: net/mlx5e: Fix kernel panic on systems like RHEL 6.x")
    Change-Id: Ief90dfec4abcc7b7c96c863ac6c0226047930e23
    Signed-off-by: Feras Daoud <ferasda@mellanox.com>

--- drivers/net/ethernet/mellanox/mlx5/core/ipoib/ipoib.c
+++ drivers/net/ethernet/mellanox/mlx5/core/ipoib/ipoib.c
@@ -92,6 +92,13 @@ unlock:
 	rtnl_unlock();
 }
 
+/* Use this function to get max num channels after netdev was created */
+static inline int mlx5i_get_netdev_max_channels(struct net_device *netdev)
+{
+	return min_t(unsigned int, netdev->num_rx_queues,
+		     netdev->num_tx_queues);
+}
+
 /* Called directly after IPoIB netdevice was created to initialize SW structs */
 int mlx5i_init(struct mlx5_core_dev *mdev,
 	       struct net_device *netdev,
@@ -110,7 +117,7 @@ int mlx5i_init(struct mlx5_core_dev *mdev,
 	netdev->mtu = netdev->max_mtu;
 
 	mlx5e_build_nic_params(mdev, &priv->rss_params, &priv->channels.params,
-			       mlx5e_get_netdev_max_channels(netdev),
+			       mlx5i_get_netdev_max_channels(netdev),
 			       netdev->mtu);
 	mlx5i_build_nic_params(mdev, &priv->channels.params);
 
@@ -140,7 +147,7 @@ void mlx5i_cleanup(struct mlx5e_priv *priv)
 
 static void mlx5i_grp_sw_update_stats(struct mlx5e_priv *priv)
 {
-	int max_nch = mlx5e_get_netdev_max_channels(priv->netdev);
+	int max_nch = mlx5i_get_netdev_max_channels(priv->netdev);
 	struct mlx5e_sw_stats s = { 0 };
 	int i, j;
 
