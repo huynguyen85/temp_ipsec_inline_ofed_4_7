3077223035b2c399d431bd52091fd5a00ee2473d
Feras Daoud <ferasda@mellanox.com>
2019-04-17 10:03:22 +0300

    IB/core: Go to host umem flow if peer client does not exist
    If peer_mem_private_data is set, but peer client does not exist,
    continue to the regular flow and try to map the host memory.
    
    Issue: 1712133
    Change-Id: Ice8136c1e515e922865bbd5eb1a6b58298958922
    Signed-off-by: Feras Daoud <ferasda@mellanox.com>

--- drivers/infiniband/core/umem.c
+++ drivers/infiniband/core/umem.c
@@ -380,8 +380,10 @@ struct ib_umem *ib_umem_get(struct ib_udata *udata, unsigned long addr,
 	/* For known peer context move directly to peer registration handling */
 	if (context->peer_mem_private_data &&
 	    (peer_mem_flags & IB_PEER_MEM_ALLOW)) {
-		ret = -EINVAL;
-		goto peer_out;
+		ret = ib_client_umem_get(context, addr, size,
+					 peer_mem_flags, umem, dmasync);
+		if (!ret)
+			return umem;
 	}
 
 	if (access & IB_ACCESS_ON_DEMAND) {
@@ -485,14 +487,14 @@ vma:
 	atomic64_sub(ib_umem_num_pages(umem), &mm->pinned_vm);
 out:
 	free_page((unsigned long) page_list);
-peer_out:
 	/*
  	 * If the address belongs to peer memory client, then the first
  	 * call to get_user_pages will fail. In this case, try to get
  	 * these pages from the peers.
  	 */
 	if (ret < 0) {
-		if (peer_mem_flags & IB_PEER_MEM_ALLOW) {
+		if ((peer_mem_flags & IB_PEER_MEM_ALLOW) && 
+		    !context->peer_mem_private_data) {
 			ret = ib_client_umem_get(context, addr, size, peer_mem_flags,
 						 umem, dmasync);
 			if (!ret)
