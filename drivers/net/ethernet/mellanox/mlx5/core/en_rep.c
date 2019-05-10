e52f0dc7f44edfd44d4ac17f0280495bc0db4387
Vlad Buslov <vladbu@mellanox.com>
2019-05-10 20:42:49 +0300

    net/mlx5e: Show/set Rx network flow classification rules on ul rep
    Reuse infrastructure that already exists for pf in legacy mode to show/set
    Rx network flow classification rules for uplink representors.
    
    issue: 1760204
    Change-Id: Idf5a88ccdf354547c229ab303bf1e950a6fcc241
    Signed-off-by: Vlad Buslov <vladbu@mellanox.com>

--- drivers/net/ethernet/mellanox/mlx5/core/en_rep.c
+++ drivers/net/ethernet/mellanox/mlx5/core/en_rep.c
@@ -284,6 +284,10 @@ static const struct ethtool_ops mlx5e_uplink_rep_ethtool_ops = {
 	.get_rxfh_indir_size = mlx5e_rep_get_rxfh_indir_size,
 	.get_rxfh          = mlx5e_get_rxfh,
 	.set_rxfh          = mlx5e_set_rxfh,
+#ifdef CONFIG_MLX5_EN_RXNFC
+	.get_rxnfc         = mlx5e_get_rxnfc,
+	.set_rxnfc         = mlx5e_set_rxnfc,
+#endif
 	.get_pauseparam    = mlx5e_uplink_rep_get_pauseparam,
 	.set_pauseparam    = mlx5e_uplink_rep_set_pauseparam,
 	.get_priv_flags    = mlx5e_get_priv_flags,
