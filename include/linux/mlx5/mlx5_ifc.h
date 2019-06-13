ccba9c958515c92c0af02d01d2ab8fdbe5d2e18a
Mark Bloch <markb@mellanox.com>
2019-06-13 22:17:55 +0000

    net/mlx5: Add IFC bits for single FDB mode
    Issue: 1706533
    Change-Id: Iddaa055bc3e5090354763dcab675b6017ed960c8
    Signed-off-by: Mark Bloch <markb@mellanox.com>

--- include/linux/mlx5/mlx5_ifc.h
+++ include/linux/mlx5/mlx5_ifc.h
@@ -728,7 +728,11 @@ struct mlx5_ifc_flow_table_eswitch_cap_bits {
 
 	struct mlx5_ifc_flow_table_prop_layout_bits flow_table_properties_esw_acl_egress;
 
-	u8      reserved_at_800[0x7800];
+	u8      reserved_at_800[0x1080];
+	u8      uplink_icm_address_rx[0x40];
+	u8      uplink_icm_address_tx[0x40];
+
+	u8      reserved_at_1900[0x6700];
 };
 
 struct mlx5_ifc_nvmf_cap_bits {
@@ -790,9 +794,10 @@ struct mlx5_ifc_e_switch_cap_bits {
 	u8         vport_cvlan_insert_overwrite[0x1];
 	u8         reserved_at_5[0x1];
 	u8         vport_cvlan_insert_always[0x1];
-	u8         reserved_at_7[0x1];
+	u8         esw_shared_ingress_acl[0x1];
 	u8         esw_uplink_ingress_acl[0x1];
-	u8         reserved_at_9[0x12];
+	u8         root_ft_on_other_esw[0x1];
+	u8         reserved_at_a[0x11];
 	u8         ecpf_vport_exists[0x1];
 	u8         counter_eswitch_affinity[0x1];
 	u8         merged_eswitch[0x1];
@@ -1329,7 +1334,8 @@ struct mlx5_ifc_cmd_hca_cap_bits {
 	u8         reserved_at_270[0x6];
 	u8         lag_dct[0x2];
 	u8         lag_tx_port_affinity[0x1];
-	u8         reserved_at_279[0x2];
+	u8         lag_native_fdb_selection[0x1];
+	u8         reserved_at_27a[0x1];
 	u8         lag_master[0x1];
 	u8         num_lag_ports[0x4];
 
@@ -3315,7 +3321,9 @@ struct mlx5_ifc_esw_vport_context_bits {
 	u8         cvlan_pcp[0x3];
 	u8         cvlan_id[0xc];
 
-	u8         reserved_at_60[0x7a0];
+	u8         reserved_at_60[0x720];
+	u8         sw_steering_vport_icm_address_rx[0x40];
+	u8         sw_steering_vport_icm_address_tx[0x40];
 };
 
 enum {
@@ -5229,7 +5237,10 @@ struct mlx5_ifc_query_hca_cap_in_bits {
 	u8         reserved_at_20[0x10];
 	u8         op_mod[0x10];
 
-	u8         reserved_at_40[0x40];
+	u8         other_function[0x1];
+	u8         reserved_at_41[0xf];
+	u8         function_id[0x10];
+	u8         reserved_at_60[0x20];
 };
 
 struct mlx5_ifc_other_hca_cap_bits {
@@ -9824,14 +9835,19 @@ struct mlx5_ifc_set_flow_table_root_in_bits {
 	u8         reserved_at_60[0x20];
 
 	u8         table_type[0x8];
-	u8         reserved_at_88[0x18];
+	u8         reserved_at_88[0x7];
+	u8         table_of_other_vport[0x1];
+	u8         table_vport_number[0x10];
 
 	u8         reserved_at_a0[0x8];
 	u8         table_id[0x18];
 
 	u8         reserved_at_c0[0x8];
 	u8         underlay_qpn[0x18];
-	u8         reserved_at_e0[0x120];
+	u8         table_eswitch_owner_vhca_id_valid[0x1];
+	u8         reserved_at_e1[0xf];
+	u8         table_eswitch_owner_vhca_id[0x10];
+	u8         reserved_at_e0[0x100];
 };
 
 enum {
@@ -10033,7 +10049,8 @@ struct mlx5_ifc_dcbx_param_bits {
 };
 
 struct mlx5_ifc_lagc_bits {
-	u8         reserved_at_0[0x1d];
+	u8         fdb_selection_mode[0x1];
+	u8         reserved_at_1[0x1c];
 	u8         lag_state[0x3];
 
 	u8         reserved_at_20[0x14];
