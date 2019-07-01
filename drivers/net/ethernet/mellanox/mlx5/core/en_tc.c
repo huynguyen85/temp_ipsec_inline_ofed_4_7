1ddc635ac3b14f136c7c72b9ac95d2dc485657ab
Jianbo Liu <jianbol@mellanox.com>
2019-07-01 10:05:29 +0000

    net/mlx5e: Don't add decap action for mirroring
    Issue: 1802092
    Change-Id: I3fc5079f0bead71bab690b740622252d6a7f3e5b
    Signed-off-by: Jianbo Liu <jianbol@mellanox.com>

--- drivers/net/ethernet/mellanox/mlx5/core/en_tc.c
+++ drivers/net/ethernet/mellanox/mlx5/core/en_tc.c
@@ -3233,7 +3233,8 @@ static int parse_tc_fdb_actions(struct mlx5e_priv *priv,
 					return -EOPNOTSUPP;
 
 #ifdef HAVE_MINIFLOW
-				if (atomic_read(&flow->flags) & MLX5E_TC_FLOW_EGRESS) {
+				if (!is_tcf_mirred_egress_mirror(a) &&
+				    (atomic_read(&flow->flags) & MLX5E_TC_FLOW_EGRESS)) {
 					action |= MLX5_FLOW_CONTEXT_ACTION_DECAP;
 				}
 #endif
