f64ae5c80c625c9ec304d743717b54f19e07fd04
Mark Zhang <markz@mellanox.com>
2019-07-02 13:02:30 +0300

    net/mlx5: Add rts2rts_qp_counters_set_id field in hca cap
    Add rts2rts_qp_counters_set_id field in hca cap so that RTS2RTS
    qp modification can be used to change the counter of a QP.
    
    Issue: 1849355
    Change-Id: I5959f2db9eb0a6e18aae94764657e59d9a9abc5a
    Signed-off-by: Mark Zhang <markz@mellanox.com>
    Reviewed-by: Majd Dibbiny <majd@mellanox.com>
    Acked-by: Saeed Mahameed <saeedm@mellanox.com>
    Signed-off-by: Leon Romanovsky <leonro@mellanox.com>

--- include/linux/mlx5/mlx5_ifc.h
+++ include/linux/mlx5/mlx5_ifc.h
@@ -1195,7 +1195,8 @@ struct mlx5_ifc_cmd_hca_cap_bits {
 	u8         cc_modify_allowed[0x1];
 	u8         start_pad[0x1];
 	u8         cache_line_128byte[0x1];
-	u8         reserved_at_165[0x5];
+	u8         reserved_at_165[0x4];
+	u8         rts2rts_qp_counters_set_id[0x1];
 	u8	   rts2rts_qp_dscp[0x1];
 	u8	   reserved_at_16b[0x4];
 	u8         qcam_reg[0x1];
