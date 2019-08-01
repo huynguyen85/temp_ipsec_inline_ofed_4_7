Shun-Hao/mlnx-ofa_kernel-4.6-1.0.1.1-rhel7.3

commit 9cc68d2f5719cffd92e354dfc8bc500e09aecbe4

Fix UAR mapping
issue:1706251
Fixes: ec983c332e8e ("IB/mlx5: Add dynamic UAR")
Change-Id: Ic1cd714b12ce810807596ee6039582332565233d
Signed-off-by: Talat Batheesh <talatb@mellanox.com>

drivers/infiniband/hw/mlx5/main_exp.c
@@ -2496,19 +2496,17 @@ int alloc_and_map_wc(struct mlx5_ib_dev *dev,
 
 
 	vma->vm_page_prot = mlx5_ib_pgprot_writecombine(vma->vm_page_prot);
-/*
-	if (io_remap_pfn_range(vma, vma->vm_start, pfn,
-			       PAGE_SIZE, vma->vm_page_prot)) {
+
+	if (rdma_user_mmap_io(&context->ibucontext, vma, pfn, map_size,
+		pgprot_writecombine(vma->vm_page_prot), vma_prv)) {
+
 		mlx5_ib_err(dev, "io remap failed\n");
 		mlx5_cmd_free_uar(dev->mdev, uar_index);
 		kfree(vma_prv);
 		return -EAGAIN;
 	}
-	context->dynamic_wc_uar_index[sys_page_idx] = uar_index;
-*/
-	rdma_user_mmap_io(&context->ibucontext, vma, pfn, map_size, pgprot_writecombine(vma->vm_page_prot),vma_prv);
-//	mlx5_ib_set_vma_data(vma, context, vma_prv);
 
+	context->dynamic_wc_uar_index[sys_page_idx] = uar_index;
 	return 0;
 }
 

huynguyen85/temp_ipsec_inline_ofed_4_7

commit 4010d41a9659b8623c403a0f9e8a2651baea6bdd

IB/{mlx5,core}: Fix broken DEVICE_GRE
DEVICE_GRE got broken on MLNX_OFED 4.6 during backport:
In the method alloc_and_map_wc(), pgrot_writecombine() is called
after mlx5_ib_pgprot_writecombine() As a result, the flags
which mlx5_ib_pgprot_writecombine() has set – are overridden

Issue: 1802398
Fixes: 9cc68d2f571 ("Fix UAR mapping")
Change-Id: I431d4e7ad145f6c6055ad98aeafd3d99216dfa7e
Signed-off-by: Yonatan Cohen <yonatanc@mellanox.com>

drivers/infiniband/hw/mlx5/main_exp.c
@@ -2432,6 +2432,7 @@ int alloc_and_map_wc(struct mlx5_ib_dev *dev,
 	u32 uar_index;
 	size_t map_size = vma->vm_end - vma->vm_start;
 	struct rdma_umap_priv *vma_prv;
+	pgprot_t vm_page_prot;
 	int err;
 
 	if (indx % uars_per_page) {
@@ -2480,11 +2481,10 @@ int alloc_and_map_wc(struct mlx5_ib_dev *dev,
 	}
 	pfn = idx2pfn(dev, uar_index);
 
+	vm_page_prot = pgprot_writecombine(vma->vm_page_prot);
+	vm_page_prot = mlx5_ib_pgprot_writecombine(vm_page_prot);
 
-	vma->vm_page_prot = mlx5_ib_pgprot_writecombine(vma->vm_page_prot);
-
-	if (rdma_user_mmap_io(&context->ibucontext, vma, pfn, map_size,
-		pgprot_writecombine(vma->vm_page_prot), vma_prv)) {
+	if (rdma_user_mmap_io(&context->ibucontext, vma, pfn, map_size, vm_page_prot, vma_prv)) {
 
 		mlx5_ib_err(dev, "io remap failed\n");
 		mlx5_cmd_free_uar(dev->mdev, uar_index);