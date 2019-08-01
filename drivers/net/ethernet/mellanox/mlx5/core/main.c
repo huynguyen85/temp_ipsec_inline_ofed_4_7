Shun-Hao/mlnx-ofa_kernel-4.6-1.0.1.1-rhel7.3

mlx5/core: Remove free IRQs from the fast shutdown call
As preparation for the fast driver unload, separate IRQs free from
the fast shutdown call and call it only if needed.
cherry-picked from 4.5-5
commit c15ee9b55e29 ("mlx5/core: Remove free IRQs from the fast shutdown call")

Issue: 1598519
Change-Id: I5886505ae3288bdaed732c6a84143e9ce9f9d1e7
Signed-off-by: Feras Daoud <ferasda@mellanox.com>

drivers/net/ethernet/mellanox/mlx5/core/main.c
@@ -2502,14 +2502,6 @@ static int mlx5_try_fast_unload(struct mlx5_core_dev *dev)
 succeed:
 	mlx5_enter_error_state(dev, true);
 
-	/* Some platforms requiring freeing the IRQ's in the shutdown
-	 * flow. If they aren't freed they can't be allocated after
-	 * kexec. There is no need to cleanup the mlx5_core software
-	 * contexts.
-	 */
-	mlx5_irq_clear_affinity_hints(dev);
-	mlx5_core_eq_free_irqs(dev);
-
 	return 0;
 }
 
@@ -2521,8 +2513,18 @@ static void shutdown(struct pci_dev *pdev)
 
 	dev_info(&pdev->dev, "Shutdown was called\n");
 	err = mlx5_try_fast_unload(dev);
-	if (err)
+	if (err) {
 		mlx5_unload_one(dev, priv, false);
+	} else {
+		/* Some platforms requiring freeing the IRQ's in the shutdown
+		 * flow. If they aren't freed they can't be allocated after
+		 * kexec. There is no need to cleanup the mlx5_core software
+		 * contexts.
+		 */
+		mlx5_irq_clear_affinity_hints(dev);
+		mlx5_core_eq_free_irqs(dev);
+	}
+
 	mlx5_pci_disable_device(dev);
 }
 

huynguyen85/temp_ipsec_inline_ofed_4_7

drivers/net/ethernet/mellanox/mlx5/core/main.c
@@ -2266,13 +2266,6 @@ static int mlx5_try_fast_unload(struct mlx5_core_dev *dev)
 succeed:
 	mlx5_enter_error_state(dev, true);
 
-	/* Some platforms requiring freeing the IRQ's in the shutdown
-	 * flow. If they aren't freed they can't be allocated after
-	 * kexec. There is no need to cleanup the mlx5_core software
-	 * contexts.
-	 */
-	mlx5_core_eq_free_irqs(dev);
-
 	return 0;
 }
 
@@ -2283,8 +2276,18 @@ static void shutdown(struct pci_dev *pdev)
 
 	mlx5_core_info(dev, "Shutdown was called\n");
 	err = mlx5_try_fast_unload(dev);
-	if (err)
+	if (err) {
 		mlx5_unload_one(dev, false);
+	} else {
+		/* Some platforms requiring freeing the IRQ's in the shutdown
+		 * flow. If they aren't freed they can't be allocated after
+		 * kexec. There is no need to cleanup the mlx5_core software
+		 * contexts.
+		 */
+		clear_comp_irqs_affinity_hints(dev);
+		mlx5_core_eq_free_irqs(dev);
+	}
+
 	mlx5_pci_disable_device(dev);
 }
 