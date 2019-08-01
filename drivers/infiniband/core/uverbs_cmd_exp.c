Shun-Hao/mlnx-ofa_kernel-4.6-1.0.1.1-rhel7.3

IB/uverbs: Add EXP verbs prefetching ODP MR pages
On demand paging allows avoiding the pinning of RDMA memory regions' pages.
However, page faults can be expensive and slow down communication
considerably. Sometimes the user program has knowledge that a coming up
communication operation might use a particular part of a memory region. In
these cases we wish to allow the user program to hint the kernel it can go
ahead and prefetch these pages, and map them into the MR, so that the page
fault can be avoided.
This adds a new verb to the uverbs module to support such a call from user
space. It accepts an MR and a memory range to prefetch, and perform roughly
what a pagefault on the same region would have done: use get user pages to
load the requested pages, and map them into the device.
This patch includes the following changes:
 - Add a new uverbs command (ib_uverbs_prefetch_mr).
 - Add a prefetch_mr function to the ib_device struc

Issue: 865148
Change-Id: I5063094459f7ee96d791e8781727701054bb77e6
Signed-off-by: Mark Bloch <markb@mellanox.com>

drivers/infiniband/core/uverbs_cmd_exp.c
@@ -611,6 +611,48 @@ err_free:
 	return ret;
 }
 
+#ifdef CONFIG_INFINIBAND_ON_DEMAND_PAGING
+int ib_uverbs_exp_prefetch_mr(struct ib_uverbs_file *file,
+			      struct ib_udata *ucore,
+			      struct ib_udata *uhw)
+{
+	struct ib_uverbs_exp_prefetch_mr  cmd;
+	struct ib_mr                     *mr;
+	int                               ret = -EINVAL;
+
+	if (ucore->inlen < sizeof(cmd))
+		return -EINVAL;
+
+	ret = ib_copy_from_udata(&cmd, ucore, sizeof(cmd));
+	if (ret)
+		return ret;
+
+	ucore->inbuf += sizeof(cmd);
+	ucore->inlen -= sizeof(cmd);
+
+	if (cmd.comp_mask)
+		return -EINVAL;
+
+	mr = uobj_get_obj_read(mr, UVERBS_OBJECT_MR, cmd.mr_handle, file);
+	if (!mr)
+		return -EINVAL;
+
+	if (!mr->device->exp_prefetch_mr)
+		return -ENOSYS;
+
+	ret = mr->device->exp_prefetch_mr(mr, cmd.start, cmd.length, cmd.flags);
+
+	uobj_put_read(mr->uobject);
+
+	if (ret)
+		return ret;
+
+	ib_umem_odp_account_prefetch_handled(file->device->ib_dev);
+
+	return ret;
+}
+#endif /* CONFIG_INFINIBAND_ON_DEMAND_PAGING */
+
 int ib_uverbs_exp_create_cq(struct ib_uverbs_file *file,
 			    struct ib_udata *ucore, struct ib_udata *uhw)
 {

huynguyen85/temp_ipsec_inline_ofed_4_7

drivers/infiniband/core/uverbs_cmd_exp.c
@@ -608,6 +608,48 @@ err_free:
 	return ret;
 }
 
+#ifdef CONFIG_INFINIBAND_ON_DEMAND_PAGING
+int ib_uverbs_exp_prefetch_mr(struct uverbs_attr_bundle *attrs)
+{
+	struct ib_uverbs_exp_prefetch_mr  cmd;
+	struct ib_mr                     *mr;
+	int                               ret = -EINVAL;
+
+	if (attrs->ucore.inlen < sizeof(cmd))
+		return -EINVAL;
+
+	ret = ib_copy_from_udata(&cmd, &attrs->ucore, sizeof(cmd));
+	if (ret)
+		return ret;
+
+	attrs->ucore.inbuf += sizeof(cmd);
+	attrs->ucore.inlen -= sizeof(cmd);
+
+	if (cmd.comp_mask)
+		return -EINVAL;
+
+	mr = uobj_get_obj_read(mr, UVERBS_OBJECT_MR, cmd.mr_handle, attrs);
+	if (!mr)
+		return -EINVAL;
+
+	if (!mr->device->ops.exp_prefetch_mr) {
+		ret = -ENOSYS;
+		goto out;
+	}
+
+	ret = mr->device->ops.exp_prefetch_mr(mr, cmd.start, cmd.length, cmd.flags);
+	if (ret)
+		goto out;
+
+	ib_umem_odp_account_prefetch_handled(mr->device);
+
+out:
+	uobj_put_read(mr->uobject);
+	return ret;
+}
+
+#endif /* CONFIG_INFINIBAND_ON_DEMAND_PAGING */
+
 int ib_uverbs_exp_create_cq(struct uverbs_attr_bundle *attrs)
 {
 	int out_len = attrs->ucore.outlen + attrs->driver_udata.outlen;