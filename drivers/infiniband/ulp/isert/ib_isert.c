Shun-Hao/mlnx-ofa_kernel-4.6-1.0.1.1-rhel7.3

commit 80b34ef915eb3fa3305ce54d518a74de8d84d761

IB/isert: Add sig_pipeline flag for isert_conn
Turn on the flag if the RDMA device supports the signature
pipeline feature. The flag will be used in the next commits.

Issue: 1050192
Change-Id: I02e5c30745f78942c68b32dbeb4a3dddec9d3183
Signed-off-by: Sergey Gorenko <sergeygo@mellanox.com>

drivers/infiniband/ulp/isert/ib_isert.c
@@ -260,11 +260,11 @@ isert_alloc_comps(struct isert_device *device)
 	device->comps_used = min(ISERT_MAX_CQ, min_t(int, num_online_cpus(),
 				 device->ib_device->num_comp_vectors));
 
-	isert_info("Using %d CQs, %s supports %d vectors support "
-		   "pi_capable %d\n",
+	isert_info("Using %d CQs, %s supports %d vectors support pi_capable %d sig_pipeline %d\n",
 		   device->comps_used, dev_name(&device->ib_device->dev),
 		   device->ib_device->num_comp_vectors,
-		   device->pi_capable);
+		   device->pi_capable,
+		   device->sig_pipeline);
 
 	device->comps = kcalloc(device->comps_used, sizeof(struct isert_comp),
 				GFP_KERNEL);
@@ -318,6 +318,9 @@ isert_create_device_ib_res(struct isert_device *device)
 	/* Check signature cap */
 	device->pi_capable = ib_dev->attrs.device_cap_flags &
 			     IB_DEVICE_SIGNATURE_HANDOVER ? true : false;
+
+	device->sig_pipeline = ib_dev->attrs.device_cap_flags &
+			       IB_DEVICE_SIGNATURE_HANDOVER ? true : false;
 
 	return 0;
 
@@ -1922,12 +1925,14 @@ isert_get_sup_prot_ops(struct iscsi_conn *conn)
 		if (device->pi_capable) {
 			isert_info("conn %p PI offload enabled\n", isert_conn);
 			isert_conn->pi_support = true;
+			isert_conn->sig_pipeline = device->sig_pipeline;
 			return TARGET_PROT_ALL;
 		}
 	}
 
 	isert_info("conn %p PI offload disabled\n", isert_conn);
 	isert_conn->pi_support = false;
+	isert_conn->sig_pipeline = false;
 
 	return TARGET_PROT_NORMAL;
 }

drivers/infiniband/ulp/isert/ib_isert.h
@@ -141,6 +141,7 @@ struct isert_conn {
 	u32			responder_resources;
 	u32			initiator_depth;
 	bool			pi_support;
+	bool			sig_pipeline;
 	struct iser_rx_desc	*login_req_buf;
 	char			*login_rsp_buf;
 	u64			login_req_dma;
@@ -183,6 +184,7 @@ struct isert_comp {
 
 struct isert_device {
 	bool			pi_capable;
+	bool			sig_pipeline;
 	int			refcount;
 	struct ib_device	*ib_device;
 	struct ib_pd		*pd;

huynguyen85/temp_ipsec_inline_ofed_4_7

commit ccad50a06dd13ce11d54324f813d6b71c5575068

IB/isert: Fix device capability checking
The IB_DEVICE_SIGNATURE_HANDOVER flag was checked instead of the
IB_DEVICE_SIGNATURE_PIPELINE flag.

Issue: 1769967
Fixes: 80b34ef915eb ("IB/isert: Add sig_pipeline flag for isert_conn")
Change-Id: Iaf511a02b6e8a47b582c7b05ec8e496208a8383f
Signed-off-by: Sergey Gorenko <sergeygo@mellanox.com>

drivers/infiniband/ulp/isert/ib_isert.c
@@ -315,7 +315,7 @@ isert_create_device_ib_res(struct isert_device *device)
 			     IB_DEVICE_SIGNATURE_HANDOVER ? true : false;
 
 	device->sig_pipeline = ib_dev->attrs.device_cap_flags &
-			       IB_DEVICE_SIGNATURE_HANDOVER ? true : false;
+			       IB_DEVICE_SIGNATURE_PIPELINE ? true : false;
 
 	return 0;
 