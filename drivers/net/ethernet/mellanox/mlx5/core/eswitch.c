Shun-Hao/mlnx-ofa_kernel-4.6-1.0.1.1-rhel7.3

commit 8eb99c417894eb52e93694ae89b7fff1b05a6e8e

net/mlx5: Fix wrong counting of vgroup0 members
The patch fixes a miscounting of the vports/vfs in
eswitch vgroup 0.

The current counting didn't reduce the number of members
when a VF was removed from group 0 and also included the PF
in the counting. The fix addresses both issues.

Issue: 1732470
Fixes: ed6fb25e9997 ("net/mlx5: Eswitch, Rate limiting per VF group")
Change-Id: Iea48ae3361c82f9af963141d471200f583581326
Signed-off-by: Ariel Levkovich <lariel@mellanox.com>

drivers/net/ethernet/mellanox/mlx5/core/eswitch.c
@@ -1841,7 +1841,7 @@ static int esw_vport_create_sched_element(struct mlx5_eswitch *esw,
 		return err;
 	}
 
-	if (group)
+	if (group && vport_num != MLX5_VPORT_PF)
 		group->num_vports++;
 
 	return 0;
@@ -1890,7 +1890,8 @@ static void esw_vport_disable_qos(struct mlx5_eswitch *esw, int vport_num)
 			 vport_num, err);
 
 	if (group) {
-		if (group->group_id && !--group->num_vports)
+		group->num_vports--;
+		if (group->group_id && !group->num_vports)
 			esw_destroy_vgroup(esw, group);
 	}
 
@@ -3121,8 +3122,9 @@ int mlx5_eswitch_vport_update_group(struct mlx5_eswitch *esw, int vport_num,
 	}
 
 	vport->info.group = group_id;
+	curr_group->num_vports--;
 
-	if (curr_group->group_id && !--curr_group->num_vports)
+	if (curr_group->group_id && !curr_group->num_vports)
 		esw_destroy_vgroup(esw, curr_group);
 	mutex_unlock(&esw->state_lock);
 

huynguyen85/temp_ipsec_inline_ofed_4_7

commit e385090319c30117e2b974b023321b46f96bde2e

net/mlx5: Decrease vgroup vport counter only on VF removal
Since we only increment the vgroup vport counter only when adding
a VF to the group decrementing the counter should follow the same
logic and avoid decrementing the PF vport when removing it from
the group.

This fix will prevent a warning print the appears due to closing
a vgroup with a vport counter that is not zero.

Issue: 1759857
Fixes: 8eb99c417894 ("net/mlx5: Fix wrong counting of vgroup0 members")
Change-Id: I1645fc8307a95cf844a14c7d38b4a3b610c6efe7
Signed-off-by: Ariel Levkovich <lariel@mellanox.com>

drivers/net/ethernet/mellanox/mlx5/core/eswitch.c
@@ -1884,7 +1884,8 @@ static void esw_vport_disable_qos(struct mlx5_eswitch *esw,
 			 vport->vport, err);
 
 	if (group) {
-		group->num_vports--;
+		if (vport->vport != MLX5_VPORT_PF)
+			group->num_vports--;
 		if (group->group_id && !group->num_vports)
 			esw_destroy_vgroup(esw, group);
 	}