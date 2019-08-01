Shun-Hao/mlnx-ofa_kernel-4.6-1.0.1.1-rhel7.3

commit fb1459a68895df2bf4a5112db49cb0523ad4de92

net/mlx5e: RX, Implement page biased-refcount optimization
Save atomic operations in page refcount accounting.
Elevate the reference count, and maintain an internal field
that indicates the refcount bias.

Performance:
Tested with ConnectX-5 25 Gbps.
CPU: Intel(R) Xeon(R) CPU E5-2697 v4 @ 2.30GHz
OS/kernel: Debian GNU/Linux 9 (4.14.0-041400-generic)

Major improvement is sensed in case the SKB creation and release
are done on different cores, as the cost of the page's atomic
refcount maintenance is high: each core flushes the value cached
in the other.

Tested single TCP stream, with application pinned to core 0
and NAPI to core 1.
To sense the differences of CPU util in BW, I disbled LRO
and increased the server's MTU to 9000 (client stays at 1500).

BW before: 19.2 Gbits/sec
BW after:  20.4 Gbits/sec (+6.25%)

Issue: 1663543
Issue: 1706130
Change-Id: Iac271ae9e485b393326333fb84b031b26993528f
Signed-off-by: Tariq Toukan <tariqt@mellanox.com>

drivers/net/ethernet/mellanox/mlx5/core/en_rx.c
@@ -193,7 +193,9 @@ static inline bool mlx5e_rx_cache_is_empty(struct mlx5e_page_cache *cache)
 static inline bool mlx5e_rx_cache_page_busy(struct mlx5e_page_cache *cache,
 					    u32 i)
 {
-	return page_ref_count(cache->page_cache[i].page) != 1;
+	struct mlx5e_dma_info *di = &cache->page_cache[i];
+
+	return (page_ref_count(di->page) - di->refcnt_bias) != 1;
 }
 
 static inline bool mlx5e_rx_cache_check_reduce(struct mlx5e_rq *rq)
@@ -300,6 +302,21 @@ static inline bool mlx5e_rx_cache_put(struct mlx5e_rq *rq,
 	return true;
 }
 
+/* Pageref elevation on page-alloc.
+ * Don't get too close to U32_MAX, keep room for other components
+ * to do further increments (SKB clones, forwarding, etc...)
+ */
+#define PAGE_REF_ELEV  (U32_MAX >> 1)
+
+/* Upper bound on number of packets that share a single page */
+#define PAGE_REF_THRSD (PAGE_SIZE / 64)
+
+static inline void page_ref_elev(struct mlx5e_dma_info *dma_info)
+{
+	page_ref_add(dma_info->page, PAGE_REF_ELEV);
+	dma_info->refcnt_bias += PAGE_REF_ELEV;
+}
+
 static inline bool mlx5e_rx_cache_get(struct mlx5e_rq *rq,
 				      struct mlx5e_dma_info *dma_info)
 {
@@ -319,6 +336,9 @@ static inline bool mlx5e_rx_cache_get(struct mlx5e_rq *rq,
 	stats->cache_reuse++;
 	*dma_info = cache->page_cache[cache->head--];
 
+	if (unlikely(page_ref_count(dma_info->page) <= PAGE_REF_THRSD))
+		page_ref_elev(dma_info);
+
 	return true;
 
 err_no_page:
@@ -335,12 +355,14 @@ static inline int mlx5e_page_alloc_mapped(struct mlx5e_rq *rq,
 		if (unlikely(!dma_info->page))
 			return -ENOMEM;
 		rq->stats->cache_alloc++;
+		dma_info->refcnt_bias = 0;
+		page_ref_elev(dma_info);
 	}
 
 	dma_info->addr = dma_map_page(rq->pdev, dma_info->page, 0,
 				      PAGE_SIZE, rq->buff.map_dir);
 	if (unlikely(dma_mapping_error(rq->pdev, dma_info->addr))) {
-		put_page(dma_info->page);
+		mlx5e_put_page(dma_info);
 		dma_info->page = NULL;
 		return -ENOMEM;
 	}
@@ -361,9 +383,10 @@ void mlx5e_page_release(struct mlx5e_rq *rq, struct mlx5e_dma_info *dma_info,
 		if (mlx5e_rx_cache_put(rq, dma_info))
 			return;
 
+		page_ref_sub(dma_info->page, dma_info->refcnt_bias);
 		page_pool_recycle_direct(rq->page_pool, dma_info->page);
 	} else {
-		put_page(dma_info->page);
+		mlx5e_put_page(dma_info);
 	}
 }
 
@@ -469,7 +492,7 @@ mlx5e_add_skb_frag(struct mlx5e_rq *rq, struct sk_buff *skb,
 	dma_sync_single_for_cpu(rq->pdev,
 				di->addr + frag_offset,
 				len, DMA_FROM_DEVICE);
-	page_ref_inc(di->page);
+	di->refcnt_bias--;
 	skb_add_rx_frag(skb, skb_shinfo(skb)->nr_frags,
 			di->page, frag_offset, len, truesize);
 }
@@ -1048,7 +1071,7 @@ mlx5e_skb_from_cqe_linear(struct mlx5e_rq *rq, struct mlx5_cqe64 *cqe,
 		return NULL;
 
 	/* queue up for recycling/reuse */
-	page_ref_inc(di->page);
+	di->refcnt_bias--;
 
 	return skb;
 }
@@ -1267,7 +1290,7 @@ mlx5e_skb_from_cqe_mpwrq_linear(struct mlx5e_rq *rq, struct mlx5e_mpw_info *wi,
 		return NULL;
 
 	/* queue up for recycling/reuse */
-	page_ref_inc(di->page);
+	di->refcnt_bias--;
 
 	return skb;
 }

drivers/infiniband/debug/memtrack.c
@@ -772,9 +772,7 @@ int is_non_trackable_alloc_func(const char *func_name)
 		"mlx4_alloc_pages",
 		"mlx4_alloc_page",
 		"mlx5e_page_alloc_mapped",
-		"mlx5e_page_release",
-		"mlx5e_rx_free_page_cache",
-		"mlx5e_rx_cache_reduce_clean_pending",
+		"mlx5e_put_page",
 		/* vnic skb functions */
 		"free_single_frag",
 		"vnic_alloc_rx_skb",

drivers/net/ethernet/mellanox/mlx5/core/en.h
@@ -459,6 +459,7 @@ struct mlx5e_txqsq {
 struct mlx5e_dma_info {
 	struct page     *page;
 	dma_addr_t      addr;
+	u32 refcnt_bias;
 };
 
 struct mlx5e_xdp_info {
@@ -572,6 +573,12 @@ struct mlx5e_page_cache {
 	u8 log_max_sz;
 	struct mlx5e_page_cache_reduce reduce;
 };
+
+static inline void mlx5e_put_page(struct mlx5e_dma_info *dma_info)
+{
+	page_ref_sub(dma_info->page, dma_info->refcnt_bias);
+	put_page(dma_info->page);
+}
 
 struct mlx5e_rq;
 typedef void (*mlx5e_fp_handle_rx_cqe)(struct mlx5e_rq*, struct mlx5_cqe64*);

drivers/net/ethernet/mellanox/mlx5/core/en/xdp.c
@@ -31,6 +31,7 @@
  */
 
 #include <linux/bpf_trace.h>
+#include <net/page_pool.h>
 #include "en/xdp.h"
 
 static inline bool
@@ -80,13 +81,16 @@ bool mlx5e_xdp_handle(struct mlx5e_rq *rq, struct mlx5e_dma_info *di,
 		__set_bit(MLX5E_RQ_FLAG_XDP_XMIT, rq->flags); /* non-atomic */
 		return true;
 	case XDP_REDIRECT:
+		mlx5e_page_dma_unmap(rq, di);
+		page_ref_sub(di->page, di->refcnt_bias);
 		/* When XDP enabled then page-refcnt==1 here */
 		err = xdp_do_redirect(rq->netdev, &xdp, prog);
-		if (unlikely(err))
+		if (unlikely(err)) {
+			page_pool_recycle_direct(rq->page_pool, di->page);
 			goto xdp_abort;
+		}
 		__set_bit(MLX5E_RQ_FLAG_XDP_XMIT, rq->flags);
 		rq->xdpsq.redirect_flush = true;
-		mlx5e_page_dma_unmap(rq, di);
 		rq->stats->xdp_redirect++;
 		return true;
 	default:

drivers/net/ethernet/mellanox/mlx5/core/en_main.c
@@ -524,7 +524,7 @@ static void mlx5e_rx_cache_reduce_clean_pending(struct mlx5e_rq *rq)
 		return;
 
 	for (i = 0; i < reduce->npages; i++)
-		put_page(reduce->pending[i].page);
+		mlx5e_put_page(&reduce->pending[i]);
 
 	clear_bit(MLX5E_RQ_STATE_CACHE_REDUCE_PENDING, &rq->state);
 }
@@ -594,11 +594,9 @@ static void mlx5e_rx_free_page_cache(struct mlx5e_rq *rq)
 	mlx5e_rx_cache_reduce_clean_pending(rq);
 	kvfree(reduce->pending);
 
-	for (i = 0; i <= cache->head; i++) {
-		struct mlx5e_dma_info *dma_info = &cache->page_cache[i];
+	for (i = 0; i <= cache->head; i++)
+		mlx5e_put_page(&cache->page_cache[i]);
 
-		put_page(dma_info->page);
-	}
 	kvfree(cache->page_cache);
 }
 

huynguyen85/temp_ipsec_inline_ofed_4_7

commit f93967aeeaf6507a97cf01f16fa047b119c79fdb

net/mlx5e: RX, decrease refcount elevation
It could be we over stepped U32 and hit a bug on.
Don't set it too close.

 page:ffffea001ddd5980 count:-2147483648 mapcount:0 mapping:0000000000000000 index:0x0
 page dumped because: VM_BUG_ON_PAGE(page_ref_count(page) <= 0)
 ------------[ cut here ]------------
 kernel BUG at ./include/linux/mm.h:945!
 RIP: 0010:skb_segment+0x1df1/0x3430
 Call Trace:
  <IRQ>
  ? pskb_extract+0x190/0x190
  ? fl_lookup+0x3d3/0x520 [cls_flower]
  tcp_gso_segment+0x2ea/0x16f0
  ? tcf_action_exec+0x129/0x2a0
  inet_gso_segment+0x4d0/0x1680
  skb_mac_gso_segment+0x1ce/0x430
  ? skb_network_protocol+0x490/0x490
  ? netif_skb_features+0x2e4/0x9d0

Issue: 1775269
Change-Id: Ifbc85d919c4438a8e437d4087a08cbd48ac8e595
Fixes: fb1459a68895 ("net/mlx5e: RX, Implement page biased-refcount optimization")
Signed-off-by: Roi Dayan <roid@mellanox.com>

drivers/net/ethernet/mellanox/mlx5/core/en_rx.c
@@ -322,7 +322,7 @@ static inline bool mlx5e_rx_cache_put(struct mlx5e_rq *rq,
  * Don't get too close to U32_MAX, keep room for other components
  * to do further increments (SKB clones, forwarding, etc...)
  */
-#define PAGE_REF_ELEV  (U32_MAX >> 1)
+#define PAGE_REF_ELEV  (U16_MAX)
 
 /* Upper bound on number of packets that share a single page */
 #define PAGE_REF_THRSD (PAGE_SIZE / 64)