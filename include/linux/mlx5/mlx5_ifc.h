Shun-Hao/mlnx-ofa_kernel-4.6-1.0.1.1-rhel7.3

net/mlx5: Enable VST with QinQ option by vport_context
Depends on esw capability to support, VST mode behavior is changed to
insert cvlan tag either if there is already vlan tag on the packet or
not. Previous VST mode behavior was to insert cvlan tag only if there
is no vlan tag already on the packet.
This change enables sending packets with two cvlan tags, the outer is
added by the eswitch.

Issue: 1407658
Change-Id: I59af93f4db4e219b071510def718025219251848
Signed-off-by: Moshe Shemesh <moshe@mellanox.com>

include/linux/mlx5/mlx5_ifc.h
@@ -673,8 +673,10 @@ struct mlx5_ifc_e_switch_cap_bits {
 	u8         vport_svlan_insert[0x1];
 	u8         vport_cvlan_insert_if_not_exist[0x1];
 	u8         vport_cvlan_insert_overwrite[0x1];
-	u8         reserved_at_5[0x18];
+	u8         reserved_at_5[0x1];
 	u8         merged_eswitch[0x1];
+	u8         vport_cvlan_insert_always[0x1];
+	u8         reserved_at_7[0x16];
 	u8         nic_vport_node_guid_modify[0x1];
 	u8         nic_vport_port_guid_modify[0x1];
 

huynguyen85/temp_ipsec_inline_ofed_4_7

include/linux/mlx5/mlx5_ifc.h
@@ -730,7 +730,9 @@ struct mlx5_ifc_e_switch_cap_bits {
 	u8         vport_svlan_insert[0x1];
 	u8         vport_cvlan_insert_if_not_exist[0x1];
 	u8         vport_cvlan_insert_overwrite[0x1];
-	u8         reserved_at_5[0x16];
+	u8         reserved_at_5[0x1];
+	u8         vport_cvlan_insert_always[0x1];
+	u8         reserved_at_7[0x14];
 	u8         ecpf_vport_exists[0x1];
 	u8         counter_eswitch_affinity[0x1];
 	u8         merged_eswitch[0x1];