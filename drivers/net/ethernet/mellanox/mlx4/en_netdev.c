Shun-Hao/mlnx-ofa_kernel-4.6-1.0.1.1-rhel7.3

net/mlx5e, net/mlx4_en: Add min/max checks for old kernels
For new kernels (with patch b80f71f ethernet/mellanox: use core
min/max MTU checking) the min/max mtu is being checked by the
kernel, but for old kernels need to keep the check in the EN drivers.

issue:1006748
Change-Id: Iabf5ecbe2a0b851adddc032e0c2abae924b16e2c
Signed-off-by: Eran Ben Elisha <eranbe@mellanox.com>

drivers/net/ethernet/mellanox/mlx4/en_netdev.c
@@ -2635,6 +2635,11 @@ static int mlx4_en_change_mtu(struct net_device *dev, int new_mtu)
 	en_dbg(DRV, priv, "Change MTU called - current:%d new:%d\n",
 		 dev->mtu, new_mtu);
 
+	if ((new_mtu < MLX4_EN_MIN_MTU) || (new_mtu > priv->max_mtu)) {
+		en_err(priv, "Bad MTU size:%d.\n", new_mtu);
+		return -EPERM;
+	}
+
 	if (priv->tx_ring_num[TX_XDP] &&
 	    !mlx4_en_check_xdp_mtu(dev, new_mtu))
 		return -EOPNOTSUPP;

huynguyen85/temp_ipsec_inline_ofed_4_7

drivers/net/ethernet/mellanox/mlx4/en_netdev.c
@@ -2635,6 +2635,11 @@ static int mlx4_en_change_mtu(struct net_device *dev, int new_mtu)
 	en_dbg(DRV, priv, "Change MTU called - current:%d new:%d\n",
 		 dev->mtu, new_mtu);
 
+	if ((new_mtu < ETH_MIN_MTU) || (new_mtu > priv->max_mtu)) {
+		en_err(priv, "Bad MTU size:%d.\n", new_mtu);
+		return -EPERM;
+	}
+
 	if (priv->tx_ring_num[TX_XDP] &&
 	    !mlx4_en_check_xdp_mtu(dev, new_mtu))
 		return -EOPNOTSUPP;