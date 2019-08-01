Shun-Hao/mlnx-ofa_kernel-4.6-1.0.1.1-rhel7.3

{IB,net}/mlx5: Handover SIGERR SQD to userspace
In signautre pipelining, when the HW detects a sigerr, it
stops at the next CI, flushes its caches, and fires an SQ_DRAIN
event to the driver to handle.

In order to handle the sigerr correctly application needs to
cancel (NOP) the pipelined send and resume the QP back to RTS.

Add QP create flag IB_QP_CREATE_SIGNATURE_PIPELINE.
Add EXP query QP verb to retrieve current CI of QP from user space.

Issue: 1709483
Change-Id: I57d7a666250855b745fbc7dccee0128f986b1098
Signed-off-by: Artemy Kovalyov <artemyko@mellanox.com>
Signed-off-by: Sergey Gorenko <sergeygo@mellanox.com>

drivers/infiniband/core/uverbs_cmd_exp.c
@@ -219,6 +219,12 @@ int ib_uverbs_exp_create_qp(struct ib_uverbs_file *file,
 				IB_QP_EXP_CREATE_PACKET_BASED_CREDIT_MODE;
 		}
 
+		if (attr->create_flags &
+		    IB_QP_EXP_USER_CREATE_SIGNATURE_PIPELINE) {
+			attr->create_flags &=
+				~IB_QP_EXP_USER_CREATE_SIGNATURE_PIPELINE;
+			attr->create_flags |= IB_QP_CREATE_SIGNATURE_PIPELINE;
+		}
 	}
 
 	if (cmd_exp->comp_mask & IB_UVERBS_EXP_CREATE_QP_QPG) {
@@ -1080,6 +1086,106 @@ int ib_uverbs_exp_create_cq(struct ib_uverbs_file *file,
 		return PTR_ERR(obj);
 
 	return 0;
+}
+
+int ib_uverbs_exp_query_qp(struct ib_uverbs_file *file,
+			   struct ib_udata *ucore, struct ib_udata *uhw)
+{
+	struct ib_uverbs_exp_query_qp		cmd;
+	struct ib_uverbs_exp_query_qp_resp	resp;
+	struct ib_qp			       *qp;
+	struct ib_qp_attr		       *attr;
+	struct ib_qp_init_attr		       *init_attr;
+	int					err;
+
+	if (ucore->outlen < sizeof(resp))
+		return -ENOSPC;
+
+	err = ib_copy_from_udata(&cmd, ucore, sizeof(cmd));
+	if (err)
+		return err;
+
+	attr = kmalloc(sizeof(*attr), GFP_KERNEL);
+	init_attr = kmalloc(sizeof(*init_attr), GFP_KERNEL);
+	if (!attr || !init_attr) {
+		err = -ENOMEM;
+		goto out;
+	}
+
+	qp = uobj_get_obj_read(qp, UVERBS_OBJECT_QP, cmd.qp_handle, file);
+	if (!qp) {
+		err = -EINVAL;
+		goto out;
+	}
+
+	err = qp->device->exp_query_qp(qp, attr, cmd.attr_mask, init_attr, uhw);
+
+	uobj_put_obj_read(qp);
+
+	if (err)
+		goto out;
+
+	memset(&resp, 0, sizeof(resp));
+	resp.qp_state		    = attr->qp_state;
+	resp.cur_qp_state	    = attr->cur_qp_state;
+	resp.path_mtu		    = attr->path_mtu;
+	resp.path_mig_state	    = attr->path_mig_state;
+	resp.qkey		    = attr->qkey;
+	resp.rq_psn		    = attr->rq_psn;
+	resp.sq_psn		    = attr->sq_psn;
+	resp.dest_qp_num	    = attr->dest_qp_num;
+	resp.qp_access_flags	    = attr->qp_access_flags;
+	resp.pkey_index		    = attr->pkey_index;
+	resp.alt_pkey_index	    = attr->alt_pkey_index;
+	resp.sq_draining	    = attr->sq_draining;
+	resp.max_rd_atomic	    = attr->max_rd_atomic;
+	resp.max_dest_rd_atomic     = attr->max_dest_rd_atomic;
+	resp.min_rnr_timer	    = attr->min_rnr_timer;
+	resp.port_num		    = attr->port_num;
+	resp.timeout		    = attr->timeout;
+	resp.retry_cnt		    = attr->retry_cnt;
+	resp.rnr_retry		    = attr->rnr_retry;
+	resp.alt_port_num	    = attr->alt_port_num;
+	resp.alt_timeout	    = attr->alt_timeout;
+
+	memcpy(resp.dest.dgid, attr->ah_attr.grh.dgid.raw, 16);
+	resp.dest.flow_label	    = attr->ah_attr.grh.flow_label;
+	resp.dest.sgid_index	    = attr->ah_attr.grh.sgid_index;
+	resp.dest.hop_limit	    = attr->ah_attr.grh.hop_limit;
+	resp.dest.traffic_class     = attr->ah_attr.grh.traffic_class;
+	resp.dest.dlid		    = attr->ah_attr.ib.dlid;
+	resp.dest.sl		    = attr->ah_attr.sl;
+	resp.dest.src_path_bits     = attr->ah_attr.ib.src_path_bits;
+	resp.dest.static_rate	    = attr->ah_attr.static_rate;
+	resp.dest.is_global	    = !!(attr->ah_attr.ah_flags & IB_AH_GRH);
+	resp.dest.port_num	    = attr->ah_attr.port_num;
+
+	memcpy(resp.alt_dest.dgid, attr->alt_ah_attr.grh.dgid.raw, 16);
+	resp.alt_dest.flow_label    = attr->alt_ah_attr.grh.flow_label;
+	resp.alt_dest.sgid_index    = attr->alt_ah_attr.grh.sgid_index;
+	resp.alt_dest.hop_limit     = attr->alt_ah_attr.grh.hop_limit;
+	resp.alt_dest.traffic_class = attr->alt_ah_attr.grh.traffic_class;
+	resp.alt_dest.dlid	    = attr->alt_ah_attr.ib.dlid;
+	resp.alt_dest.sl	    = attr->alt_ah_attr.sl;
+	resp.alt_dest.src_path_bits = attr->alt_ah_attr.ib.src_path_bits;
+	resp.alt_dest.static_rate   = attr->alt_ah_attr.static_rate;
+	resp.alt_dest.is_global     = !!(attr->alt_ah_attr.ah_flags & IB_AH_GRH);
+	resp.alt_dest.port_num	    = attr->alt_ah_attr.port_num;
+
+	resp.max_send_wr	    = init_attr->cap.max_send_wr;
+	resp.max_recv_wr	    = init_attr->cap.max_recv_wr;
+	resp.max_send_sge	    = init_attr->cap.max_send_sge;
+	resp.max_recv_sge	    = init_attr->cap.max_recv_sge;
+	resp.max_inline_data	    = init_attr->cap.max_inline_data;
+	resp.sq_sig_all		    = init_attr->sq_sig_type == IB_SIGNAL_ALL_WR;
+
+	err = ib_copy_to_udata(ucore, &resp, sizeof(resp));
+
+out:
+	kfree(init_attr);
+	kfree(attr);
+
+	return err;
 }
 
 static void set_dc_ini_ah_fields(struct ib_device *device, struct rdma_ah_attr *ah)

huynguyen85/temp_ipsec_inline_ofed_4_7

drivers/infiniband/core/uverbs_cmd_exp.c
@@ -217,6 +217,12 @@ int ib_uverbs_exp_create_qp(struct uverbs_attr_bundle *attrs)
 				IB_QP_EXP_CREATE_PACKET_BASED_CREDIT_MODE;
 		}
 
+		if (attr->create_flags &
+		    IB_QP_EXP_USER_CREATE_SIGNATURE_PIPELINE) {
+			attr->create_flags &=
+				~IB_QP_EXP_USER_CREATE_SIGNATURE_PIPELINE;
+			attr->create_flags |= IB_QP_CREATE_SIGNATURE_PIPELINE;
+		}
 	}
 
 	if (cmd_exp->comp_mask & IB_UVERBS_EXP_CREATE_QP_QPG) {
@@ -1067,6 +1073,105 @@ int ib_uverbs_exp_create_cq(struct uverbs_attr_bundle *attrs)
 		return PTR_ERR(obj);
 
 	return 0;
+}
+
+int ib_uverbs_exp_query_qp(struct uverbs_attr_bundle *attrs)
+{
+	struct ib_uverbs_exp_query_qp		cmd;
+	struct ib_uverbs_exp_query_qp_resp	resp;
+	struct ib_qp			       *qp;
+	struct ib_qp_attr		       *attr;
+	struct ib_qp_init_attr		       *init_attr;
+	int					err;
+
+	if (attrs->ucore.outlen < sizeof(resp))
+		return -ENOSPC;
+
+	err = ib_copy_from_udata(&cmd, &attrs->ucore, sizeof(cmd));
+	if (err)
+		return err;
+
+	attr = kmalloc(sizeof(*attr), GFP_KERNEL);
+	init_attr = kmalloc(sizeof(*init_attr), GFP_KERNEL);
+	if (!attr || !init_attr) {
+		err = -ENOMEM;
+		goto out;
+	}
+
+	qp = uobj_get_obj_read(qp, UVERBS_OBJECT_QP, cmd.qp_handle, attrs);
+	if (!qp) {
+		err = -EINVAL;
+		goto out;
+	}
+
+	err = qp->device->ops.exp_query_qp(qp, attr, cmd.attr_mask, init_attr, &attrs->driver_udata);
+
+	uobj_put_obj_read(qp);
+
+	if (err)
+		goto out;
+
+	memset(&resp, 0, sizeof(resp));
+	resp.qp_state		    = attr->qp_state;
+	resp.cur_qp_state	    = attr->cur_qp_state;
+	resp.path_mtu		    = attr->path_mtu;
+	resp.path_mig_state	    = attr->path_mig_state;
+	resp.qkey		    = attr->qkey;
+	resp.rq_psn		    = attr->rq_psn;
+	resp.sq_psn		    = attr->sq_psn;
+	resp.dest_qp_num	    = attr->dest_qp_num;
+	resp.qp_access_flags	    = attr->qp_access_flags;
+	resp.pkey_index		    = attr->pkey_index;
+	resp.alt_pkey_index	    = attr->alt_pkey_index;
+	resp.sq_draining	    = attr->sq_draining;
+	resp.max_rd_atomic	    = attr->max_rd_atomic;
+	resp.max_dest_rd_atomic     = attr->max_dest_rd_atomic;
+	resp.min_rnr_timer	    = attr->min_rnr_timer;
+	resp.port_num		    = attr->port_num;
+	resp.timeout		    = attr->timeout;
+	resp.retry_cnt		    = attr->retry_cnt;
+	resp.rnr_retry		    = attr->rnr_retry;
+	resp.alt_port_num	    = attr->alt_port_num;
+	resp.alt_timeout	    = attr->alt_timeout;
+
+	memcpy(resp.dest.dgid, attr->ah_attr.grh.dgid.raw, 16);
+	resp.dest.flow_label	    = attr->ah_attr.grh.flow_label;
+	resp.dest.sgid_index	    = attr->ah_attr.grh.sgid_index;
+	resp.dest.hop_limit	    = attr->ah_attr.grh.hop_limit;
+	resp.dest.traffic_class     = attr->ah_attr.grh.traffic_class;
+	resp.dest.dlid		    = attr->ah_attr.ib.dlid;
+	resp.dest.sl		    = attr->ah_attr.sl;
+	resp.dest.src_path_bits     = attr->ah_attr.ib.src_path_bits;
+	resp.dest.static_rate	    = attr->ah_attr.static_rate;
+	resp.dest.is_global	    = !!(attr->ah_attr.ah_flags & IB_AH_GRH);
+	resp.dest.port_num	    = attr->ah_attr.port_num;
+
+	memcpy(resp.alt_dest.dgid, attr->alt_ah_attr.grh.dgid.raw, 16);
+	resp.alt_dest.flow_label    = attr->alt_ah_attr.grh.flow_label;
+	resp.alt_dest.sgid_index    = attr->alt_ah_attr.grh.sgid_index;
+	resp.alt_dest.hop_limit     = attr->alt_ah_attr.grh.hop_limit;
+	resp.alt_dest.traffic_class = attr->alt_ah_attr.grh.traffic_class;
+	resp.alt_dest.dlid	    = attr->alt_ah_attr.ib.dlid;
+	resp.alt_dest.sl	    = attr->alt_ah_attr.sl;
+	resp.alt_dest.src_path_bits = attr->alt_ah_attr.ib.src_path_bits;
+	resp.alt_dest.static_rate   = attr->alt_ah_attr.static_rate;
+	resp.alt_dest.is_global     = !!(attr->alt_ah_attr.ah_flags & IB_AH_GRH);
+	resp.alt_dest.port_num	    = attr->alt_ah_attr.port_num;
+
+	resp.max_send_wr	    = init_attr->cap.max_send_wr;
+	resp.max_recv_wr	    = init_attr->cap.max_recv_wr;
+	resp.max_send_sge	    = init_attr->cap.max_send_sge;
+	resp.max_recv_sge	    = init_attr->cap.max_recv_sge;
+	resp.max_inline_data	    = init_attr->cap.max_inline_data;
+	resp.sq_sig_all		    = init_attr->sq_sig_type == IB_SIGNAL_ALL_WR;
+
+	err = ib_copy_to_udata(&attrs->ucore, &resp, sizeof(resp));
+
+out:
+	kfree(init_attr);
+	kfree(attr);
+
+	return err;
 }
 
 static void set_dc_ini_ah_fields(struct ib_device *device, struct rdma_ah_attr *ah)