Shun-Hao/mlnx-ofa_kernel-4.6-1.0.1.1-rhel7.3

IB/uverbs: Add device memory mr registration via exp_reg_mr
This change add support to register allocated device memory
buffer (DM) as MR to be accessed locally or remotely.

The uapi command of exp_reg_mr is extended to include a
new parameter - dm_handle - which is used to point the kernel
to the relevant ib_dm object for the registration.

The ib_mr_init_attr struct, used in the call to the provider's
handler of exp_reg_user_mr is extended to include a pointer to
ib_dm object and a new mr type is added - IB_MR_TYPE_DM.

Issue: 876310
Change-Id: Ic319226b58898a9f0feaa1067aebdb5ff17f8cc1
Signed-off-by: Ariel Levkovich <lariel@mellanox.com>

drivers/infiniband/core/uverbs_cmd_exp.c
@@ -798,6 +798,7 @@ int ib_uverbs_exp_reg_mr(struct ib_uverbs_file *file,
 	struct ib_uobject *uobj;
 	struct ib_pd      *pd;
 	struct ib_mr      *mr;
+	struct ib_dm	  *dm = NULL;
 	int access_flags;
 	int                ret;
 	const int min_cmd_size = offsetof(typeof(cmd), comp_mask) +
@@ -841,6 +842,23 @@ int ib_uverbs_exp_reg_mr(struct ib_uverbs_file *file,
 		goto err_free;
 	}
 
+	if (ucore->inlen >= offsetof(typeof(cmd), dm_handle) +
+		      sizeof(cmd.dm_handle) &&
+		      (cmd.comp_mask & IB_UVERBS_EXP_REG_MR_EX_DM_HANDLE)) {
+		dm = uobj_get_obj_read(dm, UVERBS_OBJECT_DM, cmd.dm_handle, file);
+		if (!dm) {
+			ret = -EINVAL;
+			goto err_put;
+		}
+
+		if (cmd.start >= dm->length) {
+			ret = -EFAULT;
+			goto err_put;
+		}
+
+		attr.mr_type = IB_MR_TYPE_DM;
+	}
+
 	if (cmd.exp_access_flags & IB_UVERBS_EXP_ACCESS_ON_DEMAND) {
 #ifdef CONFIG_INFINIBAND_ON_DEMAND_PAGING
 		struct ib_exp_device_attr exp_attr;
@@ -863,6 +881,7 @@ int ib_uverbs_exp_reg_mr(struct ib_uverbs_file *file,
 	attr.length = cmd.length;
 	attr.hca_va = cmd.hca_va;
 	attr.access_flags = access_flags;
+	attr.dm = dm;
 
 	mr = pd->device->reg_user_mr(pd, &attr, uhw);
 	if (IS_ERR(mr)) {
@@ -872,6 +891,7 @@ int ib_uverbs_exp_reg_mr(struct ib_uverbs_file *file,
 
 	mr->device  = pd->device;
 	mr->pd      = pd;
+	mr->dm	    = dm;
 	mr->uobject = uobj;
 	atomic_inc(&pd->usecnt);
 
@@ -886,6 +906,11 @@ int ib_uverbs_exp_reg_mr(struct ib_uverbs_file *file,
 	if (ret)
 		goto err_copy;
 
+	if (dm) {
+		atomic_inc(&dm->usecnt);
+		uobj_put_obj_read(dm);
+	}
+
 	uobj_put_obj_read(pd);
 
 	return uobj_alloc_commit(uobj, 0);
@@ -895,6 +920,9 @@ err_copy:
 
 err_put:
 	uobj_put_obj_read(pd);
+
+	if (dm)
+		uobj_put_obj_read(dm);
 
 err_free:
 	uobj_alloc_abort(uobj);

huynguyen85/temp_ipsec_inline_ofed_4_7

drivers/infiniband/core/uverbs_cmd_exp.c
@@ -789,6 +789,7 @@ int ib_uverbs_exp_reg_mr(struct uverbs_attr_bundle *attrs)
 	struct ib_device *ib_dev;
 	struct ib_pd      *pd;
 	struct ib_mr      *mr;
+	struct ib_dm	  *dm = NULL;
 	int access_flags;
 	int                ret;
 	const int min_cmd_size = offsetof(typeof(cmd), comp_mask) +
@@ -832,6 +833,23 @@ int ib_uverbs_exp_reg_mr(struct uverbs_attr_bundle *attrs)
 		goto err_free;
 	}
 
+	if (attrs->ucore.inlen >= offsetof(typeof(cmd), dm_handle) +
+		      sizeof(cmd.dm_handle) &&
+		      (cmd.comp_mask & IB_UVERBS_EXP_REG_MR_EX_DM_HANDLE)) {
+		dm = uobj_get_obj_read(dm, UVERBS_OBJECT_DM, cmd.dm_handle, attrs);
+		if (!dm) {
+			ret = -EINVAL;
+			goto err_put;
+		}
+
+		if (cmd.start >= dm->length) {
+			ret = -EFAULT;
+			goto err_put;
+		}
+
+		attr.mr_type = IB_MR_TYPE_DM;
+	}
+
 	if (cmd.exp_access_flags & IB_UVERBS_EXP_ACCESS_ON_DEMAND) {
 #ifdef CONFIG_INFINIBAND_ON_DEMAND_PAGING
 		struct ib_exp_device_attr exp_attr;
@@ -854,6 +872,7 @@ int ib_uverbs_exp_reg_mr(struct uverbs_attr_bundle *attrs)
 	attr.length = cmd.length;
 	attr.hca_va = cmd.hca_va;
 	attr.access_flags = access_flags;
+	attr.dm = dm;
 
 	mr = pd->device->ops.reg_user_mr(pd, &attr, &attrs->driver_udata);
 	if (IS_ERR(mr)) {
@@ -863,6 +882,7 @@ int ib_uverbs_exp_reg_mr(struct uverbs_attr_bundle *attrs)
 
 	mr->device  = pd->device;
 	mr->pd      = pd;
+	mr->dm	    = dm;
 	mr->uobject = uobj;
 	atomic_inc(&pd->usecnt);
 
@@ -877,6 +897,11 @@ int ib_uverbs_exp_reg_mr(struct uverbs_attr_bundle *attrs)
 	if (ret)
 		goto err_copy;
 
+	if (dm) {
+		atomic_inc(&dm->usecnt);
+		uobj_put_obj_read(dm);
+	}
+
 	uobj_put_obj_read(pd);
 
 	return uobj_alloc_commit(uobj, 0);
@@ -886,6 +911,9 @@ err_copy:
 
 err_put:
 	uobj_put_obj_read(pd);
+	if (dm)
+		uobj_put_obj_read(dm);
+
 
 err_free:
 	uobj_alloc_abort(uobj, attrs);