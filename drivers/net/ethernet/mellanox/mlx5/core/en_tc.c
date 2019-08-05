huynguyen85/temp_ipsec_inline_ofed_4_7

commit 953dcf85bfdc61f8cf8393216869e7d4912c7357

net/mlx5e: Check peer flow exists before taking devcom lock
Issue: 1802578
Change-Id: I044a09f16b2329a5235f7567b666f496839b0ea4
Signed-off-by: Roi Dayan <roid@mellanox.com>

drivers/net/ethernet/mellanox/mlx5/core/en_tc.c
@@ -1266,6 +1266,9 @@ static void mlx5e_tc_del_fdb_peer_flow(struct mlx5e_tc_flow *flow)
 	struct mlx5_devcom *devcom = dev->priv.devcom;
 	struct mlx5_eswitch *peer_esw;
 
+	if (!(atomic_read(&flow->flags) & MLX5E_TC_FLOW_DUP))
+		return;
+
 	peer_esw = mlx5_devcom_get_peer_data(devcom, MLX5_DEVCOM_ESW_OFFLOADS);
 	if (!peer_esw)
 		return;

commit 7d0b68bf2f3f654e100f42d013ab33b3a6a571f0

fixes: remove atomic_t call
Issue: 1824755
Fixes: 953dcf85bfdc ("net/mlx5e: Check peer flow exists before taking devcom
lock")
Change-Id: Ie594ae4dd00c1d7a70268892d17a21948ca1119d
Signed-off-by: Talat Batheesh <talatb@mellanox.com>

drivers/net/ethernet/mellanox/mlx5/core/en_tc.c
@@ -1266,9 +1266,8 @@ static void mlx5e_tc_del_fdb_peer_flow(struct mlx5e_tc_flow *flow)
 	struct mlx5_devcom *devcom = dev->priv.devcom;
 	struct mlx5_eswitch *peer_esw;
 
-//TODO VALENTINE REQUIRED Add miniflow commit
-	//if (!(atomic_read(&flow->flags) & MLX5E_TC_FLOW_DUP))
-	//	return;
+	if (!(flow->flags & MLX5E_TC_FLOW_DUP))
+		return;
 
 	peer_esw = mlx5_devcom_get_peer_data(devcom, MLX5_DEVCOM_ESW_OFFLOADS);
 	if (!peer_esw)