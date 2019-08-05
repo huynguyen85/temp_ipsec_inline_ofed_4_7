6486ff221037fba28a1d1dfc8efe03cb94c4a01d
Raed Salem <raeds@mellanox.com>
2019-08-05 12:00:19 +0300

    Revert "net/mlx5e: pass alloc flags for modify header allocation"
    This reverts commit bbabab9e84d7f3b0a3ed21dd6e94f648d4577aba.
    
    Issue: 1843978
    Change-Id: I7cd815a7057e3b087aa46d0d89c914512df6048f
    Signed-off-by: Raed Salem <raeds@mellanox.com>

--- drivers/net/ethernet/mellanox/mlx5/core/en_tc.c
+++ drivers/net/ethernet/mellanox/mlx5/core/en_tc.c
@@ -2071,8 +2071,7 @@ static int mlx5e_flow_namespace_max_modify_action(struct mlx5_core_dev *mdev,
 int alloc_mod_hdr_actions(struct mlx5e_priv *priv,
 				 struct pedit_headers_action *hdrs,
 				 int namespace,
-				 struct mlx5e_tc_flow_parse_attr *parse_attr,
-				 gfp_t flags)
+				 struct mlx5e_tc_flow_parse_attr *parse_attr)
 {
 	int nkeys, action_size, max_actions;
 
@@ -2084,7 +2083,7 @@ int alloc_mod_hdr_actions(struct mlx5e_priv *priv,
 	/* can get up to crazingly 16 HW actions in 32 bits pedit SW key */
 	max_actions = min(max_actions, nkeys * 16);
 
-	parse_attr->mod_hdr_actions = kcalloc(max_actions, action_size, flags);
+	parse_attr->mod_hdr_actions = kcalloc(max_actions, action_size, GFP_KERNEL);
 	if (!parse_attr->mod_hdr_actions)
 		return -ENOMEM;
 
@@ -2145,7 +2144,7 @@ static int alloc_tc_pedit_action(struct mlx5e_priv *priv, int namespace,
 	u8 cmd;
 
 	if (!parse_attr->mod_hdr_actions) {
-		err = alloc_mod_hdr_actions(priv, hdrs, namespace, parse_attr, GFP_KERNEL);
+		err = alloc_mod_hdr_actions(priv, hdrs, namespace, parse_attr);
 		if (err)
 			goto out_err;
 	}
