huynguyen85/temp_ipsec_inline_ofed_4_7

commit 9fe9b770e2d7238cabe877fabe9238ff8801e2d0

net/mlx5e: Expose new function for TIS destroy loop
For better modularity and code sharing.
Function internal change to be introduced in the next patches.

Issue: 1695850
Change-Id: I8d298f12dcc44832828a4ffc538597d13be7d30d
Signed-off-by: Tariq Toukan <tariqt@mellanox.com>

drivers/net/ethernet/mellanox/mlx5/core/en_main.c
@@ -3358,6 +3358,14 @@ void mlx5e_destroy_tis(struct mlx5_core_dev *mdev, u32 tisn)
 	mlx5_core_destroy_tis(mdev, tisn);
 }
 
+void mlx5e_destroy_tises(struct mlx5e_priv *priv)
+{
+	int tc;
+
+	for (tc = 0; tc < priv->profile->max_tc; tc++)
+		mlx5e_destroy_tis(priv->mdev, priv->tisn[tc]);
+}
+
 int mlx5e_create_tises(struct mlx5e_priv *priv)
 {
 	int err;
@@ -3380,11 +3388,7 @@ err_close_tises:
 
 static void mlx5e_cleanup_nic_tx(struct mlx5e_priv *priv)
 {
-	int tc;
-
-	mlx5e_tx_reporter_destroy(priv);
-	for (tc = 0; tc < priv->profile->max_tc; tc++)
-		mlx5e_destroy_tis(priv->mdev, priv->tisn[tc]);
+	mlx5e_destroy_tises(priv);
 }
 
 static void mlx5e_build_indir_tir_ctx_common(struct mlx5e_priv *priv,

drivers/net/ethernet/mellanox/mlx5/core/en.h
@@ -1213,6 +1213,7 @@ int mlx5e_create_tis(struct mlx5_core_dev *mdev, int tc,
 void mlx5e_destroy_tis(struct mlx5_core_dev *mdev, u32 tisn);
 
 int mlx5e_create_tises(struct mlx5e_priv *priv);
+void mlx5e_destroy_tises(struct mlx5e_priv *priv);
 void mlx5e_update_carrier(struct mlx5e_priv *priv);
 int mlx5e_close(struct net_device *netdev);
 int mlx5e_open(struct net_device *netdev);

drivers/net/ethernet/mellanox/mlx5/core/en_rep.c
@@ -1682,7 +1682,7 @@ static int mlx5e_init_rep_tx(struct mlx5e_priv *priv)
 {
 	struct mlx5e_rep_priv *rpriv = priv->ppriv;
 	struct mlx5_rep_uplink_priv *uplink_priv;
-	int tc, err;
+	int err;
 
 	err = mlx5e_create_tises(priv);
 	if (err) {
@@ -1717,18 +1717,15 @@ static int mlx5e_init_rep_tx(struct mlx5e_priv *priv)
 tc_esw_cleanup:
 	mlx5e_tc_esw_cleanup(priv);
 destroy_tises:
-	for (tc = 0; tc < priv->profile->max_tc; tc++)
-		mlx5e_destroy_tis(priv->mdev, priv->tisn[tc]);
+	mlx5e_destroy_tises(priv);
 	return err;
 }
 
 static void mlx5e_cleanup_rep_tx(struct mlx5e_priv *priv)
 {
 	struct mlx5e_rep_priv *rpriv = priv->ppriv;
-	int tc;
 
-	for (tc = 0; tc < priv->profile->max_tc; tc++)
-		mlx5e_destroy_tis(priv->mdev, priv->tisn[tc]);
+	mlx5e_destroy_tises(priv);
 
 	if (rpriv->rep->vport == MLX5_VPORT_UPLINK) {
 		/* clean indirect TC block notifications */

commit 5bfc06757ac141397d3d0f641a5a39b3ce31578a

net/mlx5e: Added tx reporters' destroy on tx cleanup
This patch fixes a bug in which devlink structure is realeased
from memory before the freeing the tx reporter resources.

Issue: 1856702
Fixes: 9fe9b77 ("net/mlx5e: Expose new function for TIS destroy loop")
Change-Id: I34a82922fd9780cdd9e5f831c63cc78bef8b2419
Signed-off-by: Shay Agroskin <shayag@mellanox.com>

drivers/net/ethernet/mellanox/mlx5/core/en_main.c
@@ -3395,6 +3395,7 @@ err_close_tises:
 
 static void mlx5e_cleanup_nic_tx(struct mlx5e_priv *priv)
 {
+	mlx5e_tx_reporter_destroy(priv);
 	mlx5e_destroy_tises(priv);
 }
 