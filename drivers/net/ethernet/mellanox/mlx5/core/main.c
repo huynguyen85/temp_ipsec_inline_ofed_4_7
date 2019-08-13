huynguyen85/temp_ipsec_inline_ofed_4_7

commit 2f42a496fb8ede29afcaf2dcd778078754fafc4e

net/mlx5: Add sysfs API to control RoCE enablement
Changing Roce enablement state will trigger reloading of mlx5_ib with
new state.

Add locking for roce_ib_state to prevent racing for user that have both
devlink and sysfs APIs.

Example:
echo 0 > /sys/devices/pci0000:00/0000:00:06.0/roce_enable

Issue: 1656801
Change-Id: I548347f18a9df326d985612703acf86f25282f9c
Signed-off-by: Michael Guralnik <michaelgur@mellanox.com>

drivers/net/ethernet/mellanox/mlx5/core/main.c
@@ -1793,7 +1793,7 @@ static int mlx5_mdev_init(struct mlx5_core_dev *dev, int profile_idx)
 	INIT_LIST_HEAD(&priv->bfregs.reg_head.list);
 	INIT_LIST_HEAD(&priv->bfregs.wc_head.list);
 	dev->roce.enabled = true;
-
+	mutex_init(&dev->roce.state_lock);
 	mutex_init(&priv->alloc_mutex);
 	mutex_init(&priv->pgdir_mutex);
 	INIT_LIST_HEAD(&priv->pgdir_list);
@@ -1874,6 +1874,49 @@ static void mlx5_as_notify_init(struct mlx5_core_dev *dev)
 static void mlx5_as_notify_cleanup(struct mlx5_core_dev *dev) { }
 #endif
 
+static ssize_t mlx5_roce_enable_show_enabled(struct device *device,
+					     struct device_attribute *attr,
+					     char *buf)
+{
+	struct pci_dev *pdev = container_of(device, struct pci_dev, dev);
+	struct mlx5_core_dev *dev = pci_get_drvdata(pdev);
+	int ret;
+
+	mutex_lock(&dev->roce.state_lock);
+	ret = dev->roce.enabled;
+	mutex_unlock(&dev->roce.state_lock);
+
+	return sprintf(buf, "%d\n", ret);
+}
+
+static ssize_t mlx5_roce_enable_set_enabled(struct device *device,
+					    struct device_attribute *attr,
+					    const char *buf, size_t count)
+{
+	struct pci_dev *pdev = container_of(device, struct pci_dev, dev);
+	struct mlx5_core_dev *dev = pci_get_drvdata(pdev);
+	int ret;
+	bool val;
+
+	ret = kstrtobool(buf, &val);
+	if (ret)
+		return -EINVAL;
+
+	mutex_lock(&dev->roce.state_lock);
+	dev->roce.enabled = val;
+	mlx5_reload_interface(dev, MLX5_INTERFACE_PROTOCOL_IB);
+	mutex_unlock(&dev->roce.state_lock);
+
+	return count;
+}
+
+static DEVICE_ATTR(roce_enable, 0644, mlx5_roce_enable_show_enabled,
+		   mlx5_roce_enable_set_enabled);
+
+static struct device_attribute *mlx5_roce_enable_dev_attrs =
+	&dev_attr_roce_enable;
+
+
 #define MLX5_IB_MOD "mlx5_ib"
 static int init_one(struct pci_dev *pdev, const struct pci_device_id *id)
 {
@@ -1887,6 +1930,10 @@ static int init_one(struct pci_dev *pdev, const struct pci_device_id *id)
 		dev_err(&pdev->dev, "kzalloc failed\n");
 		return -ENOMEM;
 	}
+
+	err = device_create_file(&pdev->dev, mlx5_roce_enable_dev_attrs);
+	if (err)
+		return 0;
 
 	dev = devlink_priv(devlink);
 	priv = &dev->priv;
@@ -1975,6 +2022,7 @@ static void remove_one(struct pci_dev *pdev)
 	if (pdev->is_virtfn && !priv->sriov.probe_vf)
 		goto out;
 
+	device_remove_file(&pdev->dev, mlx5_roce_enable_dev_attrs);
 	devlink_unregister(devlink);
 	mlx5_unregister_device(dev);
 

include/linux/mlx5/driver.h
@@ -808,6 +808,7 @@ struct mlx5_core_dev {
 		struct mlx5_rsvd_gids	reserved_gids;
 		u32			roce_en;
 		bool			enabled;
+		struct mutex		state_lock;
 	} roce;
 #ifdef CONFIG_MLX5_FPGA
 	struct mlx5_fpga_device *fpga;

commit 8f047f524f73b97d592a94027862f2b9cab94e31

mlx5/core: Fix init_one error flow
Free devlink when roce enable sysfs file creation fails

Issue: 1861572
Change-Id: I897ec815b7b7c3b9211ad933a396893083285d5f
Fixes: 2f42a496fb8e ("net/mlx5: Add sysfs API to control RoCE enablement")
Signed-off-by: Feras Daoud <ferasda@mellanox.com>

drivers/net/ethernet/mellanox/mlx5/core/main.c
@@ -1992,8 +1992,10 @@ static int init_one(struct pci_dev *pdev, const struct pci_device_id *id)
 	}
 
 	err = device_create_file(&pdev->dev, mlx5_roce_enable_dev_attrs);
-	if (err)
-		return 0;
+	if (err) {
+		err = 0;
+		goto mdev_init_err;
+	}
 
 	dev = devlink_priv(devlink);
 	priv = &dev->priv;