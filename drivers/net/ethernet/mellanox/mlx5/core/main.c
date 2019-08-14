huynguyen85/temp_ipsec_inline_ofed_4_7

commit 566d2abb935c95c002c085b8a62144471b47d7ca

net/mlx5: Add mdev driver to bind to mdev devices
Add a mdev driver to probe the mdev devices.

During probing mdev device,
(a) create SF device with its resources
(b) load mlx5_core and interface protocol drivers on it.

Similar remove sequence is followed during mdev device removal.

mdev device proving/removal is done by following standard kernel bus
device model.
Example:
1. Bind mdev device to mlx5_core driver.
$ echo <mdev_id> > /sys/bus/mdev/drivers/mlx5_core/bind

2. Unbind mdev device from the mlx5_core driver
$ echo <mdev_id> /sys/bus/mdev/drivers/mlx5_core/unbind

Associated netdevice and rdma device life cycle is performed with
probe() and remove() routines as part of mdev bind/unbind sequence
similar to PCI device life cycle.

Issue: 1334821
Change-Id: Ief03cfad464c4aa817c55791003c622ff8654058
Signed-off-by: Parav Pandit <parav@mellanox.com>
Signed-off-by: Vu Pham <vuhuong@mellanox.com>

drivers/net/ethernet/mellanox/mlx5/core/main.c
@@ -40,6 +40,9 @@
 #include <linux/io-mapping.h>
 #include <linux/interrupt.h>
 #include <linux/delay.h>
+#ifdef CONFIG_MLX5_MDEV
+#include <linux/mdev.h>
+#endif
 #include <linux/mlx5/driver.h>
 #include <linux/mlx5/cq.h>
 #include <linux/mlx5/qp.h>
@@ -1902,8 +1905,6 @@ int mlx5_mdev_init(struct mlx5_core_dev *dev, int profile_idx)
 	if (err)
 		goto err_pagealloc_init;
 
-	return 0;
-
 err_pagealloc_init:
 	mlx5_health_cleanup(dev);
 err_health_init:
@@ -2448,6 +2449,12 @@ static int __init init(void)
 	if (err)
 		goto err_core_dir;
 
+	err = mlx5_meddev_register_driver();
+	if (err) {
+		pci_unregister_driver(&mlx5_core_driver);
+		goto err_core_dir;
+	}
+
 #ifdef CONFIG_MLX5_CORE_EN
 	mlx5e_init();
 #endif
@@ -2463,6 +2470,8 @@ err_debug:
 
 static void __exit cleanup(void)
 {
+	mlx5_meddev_unregister_driver();
+
 #ifdef CONFIG_MLX5_CORE_EN
 	mlx5e_cleanup();
 #endif

drivers/net/ethernet/mellanox/mlx5/core/Makefile
@@ -66,4 +66,4 @@ mlx5_core-$(CONFIG_MLX5_EN_TLS) += en_accel/tls.o en_accel/tls_rxtx.o en_accel/t
 #
 # Mdev basic
 #
-mlx5_core-$(CONFIG_MLX5_MDEV) += meddev/sf.o meddev/mdev.o
+mlx5_core-$(CONFIG_MLX5_MDEV) += meddev/sf.o meddev/mdev.o meddev/mdev_driver.o

drivers/net/ethernet/mellanox/mlx5/core/meddev/mdev_driver.c
@@ -1,0 +1,41 @@
+// SPDX-License-Identifier: GPL-2.0
+// Copyright (c) 2018-19 Mellanox Technologies
+
+#include <linux/module.h>
+#include <net/devlink.h>
+#include <linux/mdev.h>
+
+#include "mlx5_core.h"
+#include "meddev/sf.h"
+
+static int mlx5_meddev_probe(struct device *dev)
+{
+	struct mdev_device *meddev = mdev_from_dev(dev);
+	struct mlx5_sf *sf = mdev_get_drvdata(meddev);
+
+	return mlx5_sf_load(sf);
+}
+
+static void mlx5_meddev_remove(struct device *dev)
+{
+	struct mdev_device *meddev = mdev_from_dev(dev);
+	struct mlx5_sf *sf = mdev_get_drvdata(meddev);
+
+	mlx5_sf_unload(sf);
+}
+
+static struct mdev_driver mlx5_meddev_driver = {
+	.name	= KBUILD_MODNAME,
+	.probe	= mlx5_meddev_probe,
+	.remove	= mlx5_meddev_remove,
+};
+
+int mlx5_meddev_register_driver(void)
+{
+	return mdev_register_driver(&mlx5_meddev_driver, THIS_MODULE);
+}
+
+void mlx5_meddev_unregister_driver(void)
+{
+	mdev_unregister_driver(&mlx5_meddev_driver);
+}

drivers/net/ethernet/mellanox/mlx5/core/mlx5_core.h
@@ -420,6 +420,9 @@ int mlx5_unload_one(struct mlx5_core_dev *dev, bool cleanup);
 void mlx5_meddev_init(struct mlx5_core_dev *dev);
 void mlx5_meddev_cleanup(struct mlx5_core_dev *dev);
 bool mlx5_medev_can_and_mark_cleanup(struct mlx5_core_dev *dev);
+
+int mlx5_meddev_register_driver(void);
+void mlx5_meddev_unregister_driver(void);
 #else
 static inline void mlx5_meddev_init(struct mlx5_core_dev *dev)
 {
@@ -432,6 +435,15 @@ static inline void mlx5_meddev_cleanup(struct mlx5_core_dev *dev)
 static inline bool mlx5_medev_can_and_mark_cleanup(struct mlx5_core_dev *dev)
 {
 	return true;
+}
+
+static inline int mlx5_meddev_register_driver(void)
+{
+	return 0;
+}
+
+static inline void mlx5_meddev_unregister_driver(void)
+{
 }
 #endif
 

commit 9f44aa1f99cd746c0442deca3e9130998998f9eb

net/mlx5: Fix merge error
Manual merge created below call trace due to missing return 0.
Fixed the merge error.

Call Trace:
 start_creating+0x5e/0x100
 debugfs_create_dir+0xc/0xe1
 mlx5_cmd_init+0x4f4/0x910 [mlx5_core]
 ? mlx5_function_setup+0xf0/0x1280 [mlx5_core]
 ? proc_alloc_inum+0x21/0x30
 ? mlx5_load_one+0x59/0xa10 [mlx5_core]
 ? proc_create_data+0x39/0x50
 ? init_one+0x26a/0x630 [mlx5_core]
 ? local_pci_probe+0x39/0x80
 ? pci_device_probe+0x143/0x1b0
 ? really_probe+0x1f7/0x3d0
 ? driver_probe_device+0x117/0x130
 ? device_driver_attach+0x4f/0x60
 ? __driver_attach+0xba/0x140
 ? device_driver_attach+0x60/0x60
 ? bus_for_each_dev+0x64/0xc0
 ? klist_add_tail+0x3b/0x60
 ? bus_add_driver+0xfe/0x210

issue: 1869034
Change-Id: I0f379a619151cdf2b8ec62b952c05cfbb64a8c78
Fixes: 566d2abb935c ("net/mlx5: Add mdev driver to bind to mdev devices")
Signed-off-by: Parav Pandit <parav@mellanox.com>

drivers/net/ethernet/mellanox/mlx5/core/main.c
@@ -1951,6 +1951,8 @@ int mlx5_mdev_init(struct mlx5_core_dev *dev, int profile_idx)
 	if (err)
 		goto err_pagealloc_init;
 
+	return 0;
+
 err_pagealloc_init:
 	mlx5_health_cleanup(dev);
 err_health_init: