huynguyen85/temp_ipsec_inline_ofed_4_7

commit 5713108b556720823d808110ceb77e6078d47c70

net/mlx5: Add hold/put rules refcount API
Steering consumers (e.g. sniffer) will need to hold a refcount
on flow rules which weren't created by them until the work on
the rule will be finished.

For that we reveal here an API to hold/put a refcount on a rule and
add a completion mechanism so the rule will not be cleared until
the ref holder will release it.

Issue: 863456
Change-Id: I7baa803e38bc66bc3aab942843261aa5dd3528e1
Signed-off-by: Maor Gottlieb <maorg@mellanox.com>

drivers/net/ethernet/mellanox/mlx5/core/fs_core.c
@@ -320,6 +320,27 @@ static int tree_remove_node(struct fs_node *node, bool locked)
 	return 0;
 }
 
+static void tree_force_remove_node(struct fs_node *node)
+{
+	struct fs_node *parent_node = node->parent;
+
+	if (node->del_hw_func)
+		node->del_hw_func(node);
+	if (parent_node) {
+		/* Only root namespace doesn't have parent and we just
+		 * need to free its node.
+		 */
+		down_write_ref_node(parent_node, false);
+		list_del_init(&node->list);
+		if (node->del_sw_func)
+			node->del_sw_func(node);
+		up_write_ref_node(parent_node, false);
+	} else {
+		kfree(node);
+	}
+	node = NULL;
+}
+
 static struct fs_prio *find_prio(struct mlx5_flow_namespace *ns,
 				 unsigned int prio)
 {
@@ -1146,6 +1167,8 @@ static struct mlx5_flow_rule *alloc_rule(struct mlx5_flow_destination *dest)
 		return NULL;
 
 	INIT_LIST_HEAD(&rule->next_ft);
+	atomic_set(&rule->users_refcount, 1);
+	init_completion(&rule->complete);
 	rule->node.type = FS_TYPE_FLOW_DEST;
 	if (dest)
 		memcpy(&rule->dest_attr, dest, sizeof(*dest));
@@ -1875,6 +1898,7 @@ EXPORT_SYMBOL(mlx5_add_flow_rules);
 void mlx5_del_flow_rules(struct mlx5_flow_handle *handle)
 {
 	struct fs_fte *fte;
+	struct mlx5_flow_rule *rule;
 	int i;
 
 	/* In order to consolidate the HW changes we lock the FTE for other
@@ -1900,6 +1924,13 @@ void mlx5_del_flow_rules(struct mlx5_flow_handle *handle)
 		del_hw_fte(&fte->node);
 		up_write(&fte->node.lock);
 		tree_put_node(&fte->node, false);
+	}
+	for (i = handle->num_rules - 1; i >= 0; i--) {
+		rule = handle->rule[i];
+		if (atomic_dec_and_test(&rule->users_refcount))
+			complete(&rule->complete);
+		wait_for_completion(&rule->complete);
+		tree_remove_node(&rule->node, false);
 	}
 	kfree(handle);
 }
@@ -2403,7 +2434,7 @@ static void clean_tree(struct fs_node *node)
 		list_for_each_entry_safe(iter, temp, &node->children, list)
 			clean_tree(iter);
 		tree_put_node(node, false);
-		tree_remove_node(node, false);
+		tree_force_remove_node(node);
 	}
 }
 
@@ -2842,3 +2873,14 @@ out:
 	return err;
 }
 EXPORT_SYMBOL(mlx5_fs_remove_rx_underlay_qpn);
+
+void mlx5_get_flow_rule(struct mlx5_flow_rule *rule)
+{
+	atomic_inc(&rule->users_refcount);
+}
+
+void mlx5_put_flow_rule(struct mlx5_flow_rule *rule)
+{
+	if (atomic_dec_and_test(&rule->users_refcount))
+		complete(&rule->complete);
+}

drivers/infiniband/hw/mlx5/main_exp.c
@@ -1210,10 +1210,10 @@ int alloc_and_map_wc(struct mlx5_ib_dev *dev,
 		mlx5_cmd_free_uar(dev->mdev, uar_index);
 		return -ENOMEM;
 	}
-/*
-	vma->vm_page_prot = pgprot_writecombine(vma->vm_page_prot);
 	pfn = idx2pfn(dev, uar_index);
 
+/*
+	vma->vm_page_prot = pgprot_writecombine(vma->vm_page_prot);
 	if (io_remap_pfn_range(vma, vma->vm_start, pfn,
 			       PAGE_SIZE, vma->vm_page_prot)) {
 		mlx5_ib_err(dev, "io remap failed\n");

drivers/net/ethernet/mellanox/mlx5/core/fs_core.h
@@ -118,6 +118,10 @@ struct mlx5_flow_rule {
 	 */
 	struct list_head			next_ft;
 	u32					sw_action;
+	/* Increased when user (sniffer) calls to mlx5_get_flow_rule */
+	atomic_t				users_refcount;
+	/* Completed when users_refcount is decremented to zero */
+	struct completion			complete;
 };
 
 struct mlx5_flow_handle {

include/linux/mlx5/fs.h
@@ -86,6 +86,7 @@ struct mlx5_flow_table;
 struct mlx5_flow_group;
 struct mlx5_flow_namespace;
 struct mlx5_flow_handle;
+struct mlx5_flow_rule;
 
 struct mlx5_flow_spec {
 	u8   match_criteria_enable;
@@ -220,6 +221,8 @@ u32 mlx5_fc_id(struct mlx5_fc *counter);
 int mlx5_fs_add_rx_underlay_qpn(struct mlx5_core_dev *dev, u32 underlay_qpn);
 int mlx5_fs_remove_rx_underlay_qpn(struct mlx5_core_dev *dev, u32 underlay_qpn);
 
+void mlx3_get_flow_rule(struct mlx5_flow_rule *rule);
+void mlx5_put_flow_rule(struct mlx5_flow_rule *rule);
 int mlx5_modify_header_alloc(struct mlx5_core_dev *dev,
 			     u8 namespace, u8 num_actions,
 			     void *modify_actions, u32 *modify_header_id);

commit 9abd856cb2c2cb48b910aa292a48c3fefa062cfd

net/mlx5: Align flow rules deletion to upstream
This change fixes a porting issue with flow rules deletaion
that causes a negative refcount. As a result the flow blocked by
waiting for completion that was never completed.

Issue: 1857398
Issue: 1858669
Change-Id: I49d539f46d627c889a1141a0585586fd82e9c7ef
Fixes: 5713108b5567 ("net/mlx5: Add hold/put rules refcount API")
Signed-off-by: Feras Daoud <ferasda@mellanox.com>

drivers/net/ethernet/mellanox/mlx5/core/fs_core.c
@@ -2078,16 +2078,6 @@ void mlx5_del_flow_rules(struct mlx5_flow_handle *handle)
 	 */
 	fs_get_obj(fte, handle->rule[0]->node.parent);
 	down_write_ref_node(&fte->node, false);
-	for (i = handle->num_rules - 1; i >= 0; i--)
-		tree_remove_node(&handle->rule[i]->node, true);
-	if (fte->modify_mask && fte->dests_size) {
-		modify_fte(fte);
-		up_write_ref_node(&fte->node, false);
-	} else {
-		del_hw_fte(&fte->node);
-		up_write(&fte->node.lock);
-		tree_put_node(&fte->node, false);
-	}
 	for (i = handle->num_rules - 1; i >= 0; i--) {
 		rule = handle->rule[i];
 		ns = get_ns(&rule->node);
@@ -2098,10 +2088,19 @@ void mlx5_del_flow_rules(struct mlx5_flow_handle *handle)
 		if (atomic_read(&rule->users_refcount) == 0)
 			complete(&rule->complete);
 		wait_for_completion(&rule->complete);
-		tree_remove_node(&rule->node, false);
+		tree_remove_node(&rule->node, true);
 		if (ns)
 			up_read(&ns->ns_rw_sem);
 	}
+	if (fte->modify_mask && fte->dests_size) {
+		modify_fte(fte);
+		up_write_ref_node(&fte->node, false);
+	} else {
+		del_hw_fte(&fte->node);
+		up_write(&fte->node.lock);
+		tree_put_node(&fte->node, false);
+	}
+
 	kfree(handle);
 }
 EXPORT_SYMBOL(mlx5_del_flow_rules);