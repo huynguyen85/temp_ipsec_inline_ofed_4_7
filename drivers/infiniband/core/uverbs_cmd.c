torvalds/linux

commit f7c8f2e9ddc71db0ae344f3ffb19df03ef32b719

IB/uverbs: Make use of ib_modify_qp variant to avoid resolving DMAC
This patch makes use of IB core's ib_modify_qp_with_udata function that
also resolves the DMAC and handles udata.

Signed-off-by: Parav Pandit <parav@mellanox.com>
Reviewed-by: Eli Cohen <eli@mellanox.com>
Reviewed-by: Daniel Jurgens <danielj@mellanox.com>
Signed-off-by: Leon Romanovsky <leon@kernel.org>
Signed-off-by: Doug Ledford <dledford@redhat.com>

drivers/infiniband/core/uverbs_cmd.c
@@ -2005,28 +2005,13 @@ static int modify_qp(struct ib_uverbs_file *file,
 	rdma_ah_set_port_num(&attr->alt_ah_attr,
 			     cmd->base.alt_dest.port_num);
 
-	if (qp->real_qp == qp) {
-		if (cmd->base.attr_mask & IB_QP_AV) {
-			ret = ib_resolve_eth_dmac(qp->device, &attr->ah_attr);
-			if (ret)
-				goto release_qp;
-		}
-		ret = ib_security_modify_qp(qp,
-					    attr,
-					    modify_qp_mask(qp->qp_type,
-							   cmd->base.attr_mask),
-					    udata);
-	} else {
-		ret = ib_security_modify_qp(qp,
-					    attr,
-					    modify_qp_mask(qp->qp_type,
-							   cmd->base.attr_mask),
-					    NULL);
-	}
+	ret = ib_modify_qp_with_udata(qp, attr,
+				      modify_qp_mask(qp->qp_type,
+						     cmd->base.attr_mask),
+				      udata);
 
 release_qp:
 	uobj_put_obj_read(qp);
-
 out:
 	kfree(attr);
 

huynguyen85/temp_ipsec_inline_ofed_4_7

commit 6f5d4de5ce9a05796f063cf5e9e19353bc7349f3

IB/uverbs: Make uverbs layer treat XRC_TGT qp as a kernel QP in modify_qp
Upstream kernel Commit f7c8f2e9ddc7 ("IB/uverbs: Make use of ib_modify_qp
variant to avoid resolving DMAC") caused uverbs to treat XRC_TGT QPs as
userspace QPs in function modify_qp() (by passing a non-NULL udata parameter
to the ib core function ib_modify_qp()).

XRC_TGT QPs are created as kernel QPs, so for them, the udata parameter in
ib_modify_qp() should be NULL.

Subsequently, upstream kernel commit 899444505473 ("IB/{hw,sw}: Remove
'uobject->context' dependency in object creation APIs") caused the low-level
drivers to decide in their xx_modify_qp() functions to treat a QP as a kernel or
userspace QP based on the udata argument in the modify_qp() function calls.
Therefore, the low-level modify_qp() functions started treating XRC_TGT QPs as
userspace QPs instead of kernel QPs.

This change broke mlnx_ofed 4.7 commit 454c85779702
("IB/mlx4: Assign user allocated DB and BF to QP"), which expects
pointer qp->uar to be non-null for userspace QPs in procedure
__mlx4_ib_modify_qp(). In this commit, qp->uar is only initialized
when creating userspace QPs. When creating kernel QPs, it remains NULL.
Therefore, when calling __mlx4_ib_modify_qp() for an XRC TGT qp
(which is treated here as a userspace QP), we see a NULL-reference Oops.

Fix this by fixing the bug in the uverbs layer: for XRC_TGT QPs, call
ib_modify_qp() with a NULL udata parameter.

issue: 1856817
Change-Id: I7a1daa72809c0298cc52d898fde1c03ef1a7e5de
Signed-off-by: Jack Morgenstein <jackm@dev.mellanox.co.il>

drivers/infiniband/core/uverbs_cmd.c
@@ -1944,10 +1944,16 @@ static int modify_qp(struct uverbs_attr_bundle *attrs,
 		copy_ah_attr_from_uverbs(qp, &attr->alt_ah_attr,
 					 &cmd->base.alt_dest);
 
-	ret = ib_modify_qp_with_udata(qp, attr,
-				      modify_qp_mask(qp->qp_type,
-						     cmd->base.attr_mask),
-				      &attrs->driver_udata);
+	if (qp->qp_type == IB_QPT_XRC_TGT)
+		ret = ib_modify_qp(qp, attr,
+				   modify_qp_mask(qp->qp_type,
+						  cmd->base.attr_mask));
+	else
+		ret =
+		ib_modify_qp_with_udata(qp, attr,
+					modify_qp_mask(qp->qp_type,
+						       cmd->base.attr_mask),
+					&attrs->driver_udata);
 
 release_qp:
 	uobj_put_obj_read(qp);