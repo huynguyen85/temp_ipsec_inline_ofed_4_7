huynguyen85/temp_ipsec_inline_ofed_4_7

commit 1855ee7f09bdbbac59e480eeaf2d2ec69ad29c19

net/mlx5e: miniflow, Link dummy counters to peer flow
Issue: 1837751
Change-Id: Iae5fad6e580d6baa8dc342941ffcc8517fa6838e
Signed-off-by: Roi Dayan <roid@mellanox.com>

drivers/net/ethernet/mellanox/mlx5/core/miniflow.c
@@ -941,9 +941,15 @@ static int __miniflow_merge(struct mlx5e_miniflow *miniflow)
 		goto err_verify;
 	}
 
-	miniflow_link_dummy_counters(miniflow->flow,
+	miniflow_link_dummy_counters(mflow,
 				     dummy_counters,
 				     miniflow->nr_flows);
+
+	if (&mflow->flags & BIT(MLX5E_TC_FLOW_FLAG_DUP))
+		miniflow_link_dummy_counters(mflow->peer_flow,
+					     dummy_counters,
+					     miniflow->nr_flows);
+
 	miniflow_attach(miniflow);
 
 	atomic_inc((atomic_t *)&currently_in_hw);

commit 7a658f8d7a38597372ab7ad662efc39cf955ca78

net/mlx5: miniflow, fix use-after-free of dummy counters
Miniflow creates nr_flows of dummy counters for its path flows, these
dummy counters is linked to the real flow counter of that miniflow.
Flow counter stats/remove will read/dealloc these dummy counters.

There is no problem until miniflow with LAG is introduced, as the peer
miniflow also links the exactly same dummy counters with peer flow
counters.

So, there are two users for the same resource without knowing each
other. The use-after-free happens when:
1. One flow is deleted and hence deallocate the dummy counters it
   linked with.
2. The other flow is not deleted yet but still try to update the
   stats and manipulate the part of cache which is already freed.

This memory corruption caused unknown kernel crashes as it touches
memory which might be already assigned to other user(e.g, skb).

Solution is to unlink the counters from peer flow as well.

Issue: 1844417
Change-Id: I93ae4fe38c89d75c3e342eab500426d3ff61dcac
Fixes: 1855ee7f09bd ("net/mlx5e: miniflow, Link dummy counters to peer flow")
Signed-off-by: Bodong Wang <bodong@mellanox.com>

drivers/net/ethernet/mellanox/mlx5/core/miniflow.c
@@ -1053,6 +1053,9 @@ void mlx5e_del_miniflow_list(struct mlx5e_tc_flow *flow)
 	list_for_each_entry_safe(mnode, n, &flow->miniflow_list, node) {
 		struct mlx5e_miniflow *miniflow = mnode->miniflow;
 
+		if (atomic_read(&miniflow->flow->flags) & MLX5E_TC_FLOW_DUP)
+			miniflow_unlink_dummy_counters(miniflow->flow->peer_flow);
+
 		miniflow_unlink_dummy_counters(miniflow->flow);
 		miniflow_detach(miniflow);
 