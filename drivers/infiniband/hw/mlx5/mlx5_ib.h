e7796cab513306d67286681fda75f008e4df5386
Mark Zhang <markz@mellanox.com>
2019-08-27 05:44:40 +0300

    RDMA/mlx5: RDMA_RX flow type support for user applications
    Currently user applications can only steer TCP/IP(NIC RX/RX) traffic.
    This patch adds RDMA_RX as a new flow type to allow the user to insert
    steering rules to control RDMA traffic.
    Two destinations are supported(but not set at the same time): devx
    flow table object and QP.
    
    Issue: 1888575
    Issue: 1888576
    Change-Id: I7bbd3275ba34d2bbfa8fa96fa2e819ff0be5b484
    Signed-off-by: Mark Zhang <markz@mellanox.com>

--- drivers/infiniband/hw/mlx5/mlx5_ib.h
+++ drivers/infiniband/hw/mlx5/mlx5_ib.h
@@ -216,6 +216,7 @@ struct mlx5_ib_flow_db {
 	struct mlx5_ib_flow_prio	sniffer[MLX5_IB_NUM_SNIFFER_FTS];
 	struct mlx5_ib_flow_prio	egress[MLX5_IB_NUM_EGRESS_FTS];
 	struct mlx5_ib_flow_prio	fdb;
+	struct mlx5_ib_flow_prio	rdma_rx[MLX5_IB_NUM_FLOW_FT];
 	struct mlx5_flow_table		*lag_demux_ft;
 	struct mlx5_steering_data steering_data[MLX5_MAX_PORTS];
 	/* Protect flow steering bypass flow tables
