huynguyen85/temp_ipsec_inline_ofed_4_7

commit c215e2e3929cbb1b3af1ab4bbe73359f64529afd

net/mlx5_core: Add sysfs to control RoCE LAG
Add the following sysfs entry:
	/sys/bus/pci/drivers/mlx5_core/<bdf>/roce_lag_enable

Users can write 0 to disable RoCE LAG and 1 to enable it.

issue: 1092906
Change-Id: I3ddf9b5a281e4637368d0e907a75505e9c464950
Signed-off-by: Majd Dibbiny <majd@mellanox.com>

drivers/net/ethernet/mellanox/mlx5/core/lag.c
@@ -555,6 +555,58 @@ static void mlx5_lag_dev_remove_pf(struct mlx5_lag *ldev,
 	mutex_unlock(&lag_mutex);
 }
 
+static bool mlx5_is_lag_allowed(struct mlx5_core_dev *dev)
+{
+	struct mlx5_lag *ldev;
+	bool allowed = false;
+
+	mutex_lock(&lag_mutex);
+	ldev = mlx5_lag_dev_get(dev);
+	if (ldev)
+		allowed = mlx5_lag_check_prereq(ldev);
+	mutex_unlock(&lag_mutex);
+
+	return allowed;
+}
+
+static ssize_t mlx5_lag_show_enabled(struct device *device,
+				     struct device_attribute *attr,
+				     char *buf)
+{
+	struct pci_dev *pdev = container_of(device, struct pci_dev, dev);
+	struct mlx5_core_dev *dev  = pci_get_drvdata(pdev);
+
+	return sprintf(buf, "%d\n", mlx5_is_lag_allowed(dev) ? 1 : 0);
+}
+
+static ssize_t mlx5_lag_set_enabled(struct device *device,
+				    struct device_attribute *attr,
+				    const char *buf, size_t count)
+{
+	struct pci_dev *pdev = container_of(device, struct pci_dev, dev);
+	struct mlx5_core_dev *dev  = pci_get_drvdata(pdev);
+	int ret = -EINVAL;
+	u32 val;
+
+	ret = kstrtou32(buf, 0, &val);
+	if (ret)
+		return ret;
+
+	if (val == 1)
+		dev->priv.lag_disabled = 0;
+	else if (val == 0)
+		dev->priv.lag_disabled = 1;
+	else
+		return -EINVAL;
+
+	mlx5_lag_update(dev);
+
+	return ret ? ret : count;
+}
+
+static DEVICE_ATTR(roce_lag_enable, 0644, mlx5_lag_show_enabled, mlx5_lag_set_enabled);
+static struct device_attribute *mlx5_lag_dev_attrs = &dev_attr_roce_lag_enable;
+
 /* Must be called with intf_mutex held */
 void mlx5_lag_add(struct mlx5_core_dev *dev, struct net_device *netdev)
 {
@@ -567,6 +619,11 @@ void mlx5_lag_add(struct mlx5_core_dev *dev, struct net_device *netdev)
 	    (MLX5_CAP_GEN(dev, num_lag_ports) != MLX5_MAX_PORTS))
 		return;
 
+	if (device_create_file(&dev->pdev->dev, mlx5_lag_dev_attrs)) {
+		mlx5_core_err(dev, "Failed to create RoCE LAG sysfs\n");
+		return;
+	}
+
 	tmp_dev = mlx5_get_next_phys_dev(dev);
 	if (tmp_dev)
 		ldev = tmp_dev->priv.lag;
@@ -575,7 +632,7 @@ void mlx5_lag_add(struct mlx5_core_dev *dev, struct net_device *netdev)
 		ldev = mlx5_lag_dev_alloc();
 		if (!ldev) {
 			mlx5_core_err(dev, "Failed to alloc lag dev\n");
-			return;
+			goto remove_file;
 		}
 	}
 
@@ -593,6 +650,11 @@ void mlx5_lag_add(struct mlx5_core_dev *dev, struct net_device *netdev)
 	if (err)
 		mlx5_core_err(dev, "Failed to init multipath lag err=%d\n",
 			      err);
+
+	return;
+
+remove_file:
+	device_remove_file(&dev->pdev->dev, mlx5_lag_dev_attrs);
 }
 
 /* Must be called with intf_mutex held */
@@ -604,6 +666,8 @@ void mlx5_lag_remove(struct mlx5_core_dev *dev)
 	ldev = mlx5_lag_dev_get(dev);
 	if (!ldev)
 		return;
+
+	device_remove_file(&dev->pdev->dev, mlx5_lag_dev_attrs);
 
 	if (__mlx5_lag_is_active(ldev))
 		mlx5_deactivate_lag(ldev);

include/linux/mlx5/driver.h
@@ -621,6 +621,11 @@ struct mlx5_priv {
 	spinlock_t              ctx_lock;
 	struct mlx5_events      *events;
 
+	/* Used for Disabel RoCE LAG sysfs */
+	bool                    lag_disabled;
+	/* Used to save RoCE LAG state during recovery flow */
+	bool                    lag_enabled;
+
 	struct mlx5_flow_steering *steering;
 	struct mlx5_mpfs        *mpfs;
 	struct mlx5_eswitch     *eswitch;

commit 19c9cd578a7113ea6308f644be226c3b152b2052

mlx5/core: Fix RoCE LAG disable
Before this change, it was possible to ignore the sysfs setting roce_lag_enable
so the RoCE lag is enabled even though roce_lag_enable is set to false.
Fix by adding a check for this parameter in case of RoCE lag can be
enabled.

issue: 1887740
Change-Id: I11448a70e5bb620b25a58c569f663e7d507672da
Fixes: c215e2e3929c ("net/mlx5_core: Add sysfs to control RoCE LAG")
Signed-off-by: Danit Goldberg <danitg@mellanox.com>

drivers/net/ethernet/mellanox/mlx5/core/lag.c
@@ -284,14 +284,28 @@ static int mlx5_deactivate_lag(struct mlx5_lag *ldev)
 
 static bool mlx5_lag_check_prereq(struct mlx5_lag *ldev)
 {
-	if (!ldev->pf[0].dev || !ldev->pf[1].dev)
+	struct mlx5_core_dev *dev0 = ldev->pf[0].dev;
+	struct mlx5_core_dev *dev1 = ldev->pf[1].dev;
+	bool roce_lag_allowed;
+
+	if (!dev0 || !dev1)
 		return false;
+       roce_lag_allowed = !mlx5_sriov_is_enabled(dev0) &&
+			  !mlx5_sriov_is_enabled(dev1);
 
 #ifdef CONFIG_MLX5_ESWITCH
-	return mlx5_esw_lag_prereq(ldev->pf[0].dev, ldev->pf[1].dev);
+	roce_lag_allowed &= dev0->priv.eswitch->mode == SRIOV_NONE &&
+		dev1->priv.eswitch->mode == SRIOV_NONE;
+#endif
+
+	if (roce_lag_allowed)
+		return !dev0->priv.lag_disabled && !dev1->priv.lag_disabled;
+
+#ifdef CONFIG_MLX5_ESWITCH
+	return mlx5_esw_lag_prereq(dev0, dev1);
 #else
-	return (!mlx5_sriov_is_enabled(ldev->pf[0].dev) &&
-		!mlx5_sriov_is_enabled(ldev->pf[1].dev));
+	return (!mlx5_sriov_is_enabled(dev0) &&
+		!mlx5_sriov_is_enabled(dev1));
 #endif
 }
 
@@ -649,20 +663,6 @@ static void mlx5_lag_dev_remove_pf(struct mlx5_lag *ldev,
 	mutex_unlock(&lag_mutex);
 }
 
-static bool mlx5_is_lag_allowed(struct mlx5_core_dev *dev)
-{
-	struct mlx5_lag *ldev;
-	bool allowed = false;
-
-	mutex_lock(&lag_mutex);
-	ldev = mlx5_lag_dev_get(dev);
-	if (ldev)
-		allowed = mlx5_lag_check_prereq(ldev);
-	mutex_unlock(&lag_mutex);
-
-	return allowed;
-}
-
 static ssize_t mlx5_lag_show_enabled(struct device *device,
 				     struct device_attribute *attr,
 				     char *buf)
@@ -670,7 +670,7 @@ static ssize_t mlx5_lag_show_enabled(struct device *device,
 	struct pci_dev *pdev = container_of(device, struct pci_dev, dev);
 	struct mlx5_core_dev *dev  = pci_get_drvdata(pdev);
 
-	return sprintf(buf, "%d\n", mlx5_is_lag_allowed(dev) ? 1 : 0);
+	return sprintf(buf, "%d\n", !dev->priv.lag_disabled);
 }
 
 static ssize_t mlx5_lag_set_enabled(struct device *device,