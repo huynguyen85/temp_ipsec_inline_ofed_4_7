Shun-Hao/mlnx-ofa_kernel-4.6-1.0.1.1-rhel7.3

commit 408537760a15318d414ae1855e07fc6ed7d46dff

IB/mlx5: CQE zip
This patch enable mlx5 to use CQE zipping. After this patch kernel
application still can't use this feature

Issue: 865103
Change-Id: I370b570b61d251c6230b285e0f7ce73ad7f67de5
Signed-off-by: Haggai Abramovsky <hagaya@mellanox.com>
Signed-off-by: Maor Gottlieb <maorg@mellanox.com>

drivers/infiniband/hw/mlx5/cq.c
@@ -35,6 +35,7 @@
 #include <rdma/ib_user_verbs.h>
 #include <rdma/ib_cache.h>
 #include "mlx5_ib.h"
+#include "user_exp.h"
 
 static void mlx5_ib_cq_comp(struct mlx5_core_cq *cq)
 {
@@ -428,6 +429,24 @@ static void get_sig_err_item(struct mlx5_sig_err_cqe *cqe,
 	item->key = be32_to_cpu(cqe->mkey);
 }
 
+static void set_cqe_compression(struct mlx5_ib_dev *dev,
+				struct mlx5_exp_ib_create_cq *ucmd,
+				u32 **cqb)
+{
+	if (ucmd->cqe_size  == 64 && MLX5_CAP_GEN(dev->mdev, cqe_compression)) {
+		if (ucmd->exp_data.cqe_comp_en == 1 &&
+		    (ucmd->exp_data.comp_mask & MLX5_EXP_CREATE_CQ_MASK_CQE_COMP_EN)) {
+			MLX5_SET(cqc, *cqb, cqe_comp_en, 1);
+			if (ucmd->exp_data.cqe_comp_recv_type ==
+			    MLX5_IB_CQE_FORMAT_CSUM &&
+			    (ucmd->exp_data.comp_mask &
+			     MLX5_EXP_CREATE_CQ_MASK_CQE_COMP_RECV_TYPE))
+				MLX5_SET(cqc, *cqb, mini_cqe_res_format,
+					 MLX5_IB_CQE_FORMAT_CSUM);
+		}
+	}
+}
+
 static void sw_send_comp(struct mlx5_ib_qp *qp, int num_entries,
 			 struct ib_wc *wc, int *npolled)
 {
@@ -775,7 +794,7 @@ static int create_cq_user(struct mlx5_ib_dev *dev, struct ib_udata *udata,
 			  int entries, u32 **cqb,
 			  int *cqe_size, int *index, int *inlen)
 {
-	struct mlx5_ib_create_cq ucmd = {};
+	struct mlx5_exp_ib_create_cq ucmd = {};
 	size_t ucmdlen;
 	int page_shift;
 	__be64 *pas;
@@ -784,8 +803,14 @@ static int create_cq_user(struct mlx5_ib_dev *dev, struct ib_udata *udata,
 	void *cqc;
 	int err;
 
-	ucmdlen = udata->inlen < sizeof(ucmd) ?
-		  (sizeof(ucmd) - sizeof(ucmd.flags)) : sizeof(ucmd);
+	memset(&ucmd, 0, sizeof(ucmd));
+
+	if (udata->src == IB_UDATA_EXP_CMD)
+		ucmdlen = min(sizeof(ucmd), udata->inlen);
+	else
+		ucmdlen = udata->inlen < sizeof(struct mlx5_ib_create_cq) ?
+			(sizeof(struct mlx5_ib_create_cq) - sizeof(ucmd.flags)) :
+			sizeof(ucmd);
 
 	if (ib_copy_from_udata(&ucmd, udata, ucmdlen))
 		return -EFAULT;
@@ -859,6 +884,8 @@ static int create_cq_user(struct mlx5_ib_dev *dev, struct ib_udata *udata,
 
 		MLX5_SET(cqc, cqc, cqe_comp_en, 1);
 		MLX5_SET(cqc, cqc, mini_cqe_res_format, mini_cqe_format);
+	} else {
+		set_cqe_compression(dev, &ucmd, cqb);
 	}
 
 	if (ucmd.flags & MLX5_IB_CREATE_CQ_FLAGS_CQE_128B_PAD) {

drivers/infiniband/hw/mlx5/main.c
@@ -61,6 +61,7 @@
 #include "mlx5_ib.h"
 #include "ib_rep.h"
 #include "cmd.h"
+#include "user_exp.h"
 #include <linux/mlx5/fs_helpers.h>
 #include <linux/mlx5/accel.h>
 #include <rdma/uverbs_std_types.h>
@@ -1650,12 +1651,22 @@ static void mlx5_ib_dealloc_transport_domain(struct mlx5_ib_dev *dev, u32 tdn,
 	mlx5_ib_disable_lb(dev, true, false);
 }
 
+static void set_exp_data(struct mlx5_ib_dev *dev,
+			 struct mlx5_exp_ib_alloc_ucontext_resp *resp)
+{
+	resp->exp_data.comp_mask = MLX5_EXP_ALLOC_CTX_RESP_MASK_CQE_VERSION |
+		MLX5_EXP_ALLOC_CTX_RESP_MASK_CQE_COMP_MAX_NUM;
+	resp->exp_data.cqe_version = MLX5_CAP_GEN(dev->mdev, cqe_version);
+	resp->exp_data.cqe_comp_max_num = MLX5_CAP_GEN(dev->mdev,
+						      cqe_compression_max_num);
+}
+
 static struct ib_ucontext *mlx5_ib_alloc_ucontext(struct ib_device *ibdev,
 						  struct ib_udata *udata)
 {
 	struct mlx5_ib_dev *dev = to_mdev(ibdev);
 	struct mlx5_ib_alloc_ucontext_req_v2 req = {};
-	struct mlx5_ib_alloc_ucontext_resp resp = {};
+	struct mlx5_exp_ib_alloc_ucontext_resp resp = {};
 	struct mlx5_core_dev *mdev = dev->mdev;
 	struct mlx5_ib_ucontext *context;
 	struct mlx5_bfreg_info *bfregi;
@@ -1840,6 +1851,12 @@ static struct ib_ucontext *mlx5_ib_alloc_ucontext(struct ib_device *ibdev,
 				MLX5_IB_ALLOC_UCONTEXT_RESP_MASK_DUMP_FILL_MKEY;
 		}
 		resp.response_length += sizeof(resp.dump_fill_mkey);
+	}
+
+	if (field_avail(typeof(resp), exp_data, udata->outlen)) {
+		set_exp_data(dev, &resp);
+		resp.response_length = min_t(size_t, udata->outlen,
+					     sizeof(resp));
 	}
 
 	err = ib_copy_to_udata(udata, &resp, resp.response_length);

drivers/infiniband/hw/mlx5/user_exp.h
@@ -35,6 +35,81 @@
 
 #include <rdma/mlx5-abi.h>
 
+enum mlx5_exp_ib_create_cq_mask {
+	MLX5_EXP_CREATE_CQ_MASK_CQE_COMP_EN		= 1 << 0,
+	MLX5_EXP_CREATE_CQ_MASK_CQE_COMP_RECV_TYPE      = 1 << 1,
+	MLX5_EXP_CREATE_CQ_MASK_RESERVED		= 1 << 2,
+};
+
+enum mlx5_exp_cqe_comp_recv_type {
+	MLX5_IB_CQE_FORMAT_HASH,
+	MLX5_IB_CQE_FORMAT_CSUM,
+};
+
+struct mlx5_exp_ib_create_cq_data {
+	__u32   comp_mask; /* use mlx5_exp_ib_creaet_cq_mask */
+	__u8    cqe_comp_en;
+	__u8    cqe_comp_recv_type; /* use mlx5_exp_cqe_comp_recv_type */
+	__u16	reserved;
+};
+
+struct mlx5_exp_ib_create_cq {
+	__u64					buf_addr;
+	__u64					db_addr;
+	__u32					cqe_size;
+	__u8					cqe_comp_en;
+	__u8					cqe_comp_res_format;
+	__u16					flags; /* Use mlx5_ib_create_cq_flags */
+
+	/* Some more reserved fields for future growth of mlx5_ib_create_cq */
+	__u64					prefix_reserved[8];
+
+	/* sizeof prefix aligned with mlx5_ib_create_cq */
+	__u64					size_of_prefix;
+	struct mlx5_exp_ib_create_cq_data	exp_data;
+};
+
+enum mlx5_exp_ib_alloc_ucontext_data_resp_mask {
+	MLX5_EXP_ALLOC_CTX_RESP_MASK_CQE_COMP_MAX_NUM	= 1 << 0,
+	MLX5_EXP_ALLOC_CTX_RESP_MASK_CQE_VERSION	= 1 << 1,
+};
+
+struct mlx5_exp_ib_alloc_ucontext_data_resp {
+	__u32   comp_mask; /* use mlx5_ib_exp_alloc_ucontext_data_resp_mask */
+	__u16	cqe_comp_max_num;
+	__u8	cqe_version;
+	__u8	reserved;
+};
+
+struct mlx5_exp_ib_alloc_ucontext_resp {
+	__u32						qp_tab_size;
+	__u32						bf_reg_size;
+	__u32						tot_bfregs;
+	__u32						cache_line_size;
+	__u16						max_sq_desc_sz;
+	__u16						max_rq_desc_sz;
+	__u32						max_send_wqebb;
+	__u32						max_recv_wr;
+	__u32						max_srq_recv_wr;
+	__u16						num_ports;
+	__u16						flow_action_flags;
+	__u32						comp_mask;
+	__u32						response_length;
+	__u8						cqe_version;
+	__u8						cmds_supp_uhw;
+	__u8						eth_min_inline;
+	__u8						clock_info_versions;
+	__u64						hca_core_clock_offset;
+	__u32						log_uar_size;
+	__u32						num_uars_per_page;
+	__u32						num_dyn_bfregs;
+	__u32                                           dump_fill_mkey;
+	__u32						reserved3;
+	/* Some more reserved fields for future growth of mlx5_ib_alloc_ucontext_resp */
+	__u64						prefix_reserved[8];
+	struct mlx5_exp_ib_alloc_ucontext_data_resp	exp_data;
+};
+
 enum mlx5_exp_ib_create_qp_mask {
 	MLX5_EXP_CREATE_QP_MASK_UIDX		= 1 << 0,
 	MLX5_EXP_CREATE_QP_MASK_RESERVED	= 1 << 1,

huynguyen85/temp_ipsec_inline_ofed_4_7

commit 4ead20f3741f1bc7325808531450762c35655ff5

IB/mlx5: Copy create_cq udata from user space with correct length
When the exp verb is not used we need to copy data with size
"sizeof(struct mlx5_ib_create_cq)" at maximum, not "sizeof(ucmd)" which
is much larger.

Issue: 1886443
Fixes: 408537760a15 ("IB/mlx5: CQE zip")
Change-Id: I6e5fe915fddeaeff4c4ea7a525f657acb74656a6
Signed-off-by: Mark Zhang <markz@mellanox.com>

drivers/infiniband/hw/mlx5/cq.c
@@ -734,7 +734,7 @@ static int create_cq_user(struct mlx5_ib_dev *dev, struct ib_udata *udata,
 	else
 		ucmdlen = udata->inlen < sizeof(struct mlx5_ib_create_cq) ?
 			(sizeof(struct mlx5_ib_create_cq) - sizeof(ucmd.flags)) :
-			sizeof(ucmd);
+			sizeof(struct mlx5_ib_create_cq);
 
 	if (ib_copy_from_udata(&ucmd, udata, ucmdlen))
 		return -EFAULT;