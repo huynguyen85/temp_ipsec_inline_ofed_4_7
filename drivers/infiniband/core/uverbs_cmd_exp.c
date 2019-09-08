huynguyen85/temp_ipsec_inline_ofed_4_7

commit cecbb5018ceb82b99d06f3bffc080a04cff0fc72

IB/uverbs: Add experimental QPs to RDMA resource tracker
Experimental QPs have a different creation flow, and thus they weren't added to
the resource tracker.

issue: 1339055
Change-Id: I5ae8f2051d26637c3fc88a5662ecef10f12a7b23
Signed-off-by: Majd Dibbiny <majd@mellanox.com>

drivers/infiniband/core/uverbs_cmd_exp.c
@@ -307,6 +307,17 @@ int ib_uverbs_exp_create_qp(struct uverbs_attr_bundle *attrs)
 	}
 	qp->uobject = &obj->uevent.uobject;
 
+	/*
+	 * We don't track XRC QPs for now, because they don't have PD
+	 * and more importantly they are created internaly by driver,
+	 * see mlx5 create_dev_resources() as an example.
+	 */
+	qp->res.type = RDMA_RESTRACK_QP;
+	if (attr->qp_type < IB_QPT_XRC_INI)
+		rdma_restrack_kadd(&qp->res);
+	else
+		rdma_restrack_dontrack(&qp->res);
+
 	obj->uevent.uobject.object = qp;
 
 	memset(&resp_exp, 0, sizeof(resp_exp));

commit 1b7039acd8adecb32e424f999ab8345b16a24150

RDMA/mlx5: Track QPs created with exp_create_qp as user resources
A QP created with the exp_create_qp verb must be tracked as a user resource,
otherwise it's kern_name (which is inheritted from the PD, which usually is
a user resource) would be NULL. In this case kernel crashes when query the QP
information with a user command like "rdma res show qp":

[ 1989.254327] ------------10.135.120.1:55339------------
[ 1990.392639] { 'uptime_diff_signature' : 1989.41, 'current_time' : datetime.datetime(2019, 8, 7, 11, 12, 56, 801879) }
[ 2446.312304] BUG: kernel NULL pointer dereference, address: 0000000000000000
[ 2446.313822] #PF: supervisor read access in kernel mode
[ 2446.315323] #PF: error_code(0x0000) - not-present page
[ 2446.316750] PGD 800000062f5df067 P4D 800000062f5df067 PUD 50cb4a067 PMD 0
[ 2446.318211] Oops: 0000 [#1] SMP PTI
[ 2446.319713] CPU: 12 PID: 7710 Comm: rdma Tainted: G           OE     5.2.0-mlnx #1
[ 2446.321193] Hardware name: Dell Inc. PowerEdge R720/0M1GCR, BIOS 2.5.4 01/22/2016
[ 2446.322697] RIP: 0010:strlen+0x0/0x20
[ 2446.324173] Code: f6 82 c0 5b 13 82 20 74 10 48 83 c0 01 0f b6 10 f6 82 c0 5b 13 82 20 75 f0 f3 c3 66 66 66 66 66 66 2e 0f 1f 84 00 00 00 00 00 <80> 3f 00 74 10 48 89 f8 48 83 c0 01 80 38 00 75 f7 48 29 f8 c3 31
[ 2446.327337] RSP: 0018:ffffc9000833f740 EFLAGS: 00010246
[ 2446.328924] RAX: 0000000000000000 RBX: ffff888642f52b00 RCX: 0000000000000000
[ 2446.330550] RDX: 0000000000000001 RSI: ffff888bea2ed8d8 RDI: 0000000000000000
[ 2446.332164] RBP: 0000000000000000 R08: 0000000000000db4 R09: ffff88863f6c9111
[ 2446.333784] R10: 0000000000000000 R11: ffffea002fb84980 R12: ffffc9000833f774
[ 2446.335422] R13: ffff888c3b780000 R14: 0000000000000000 R15: ffff888bea2ed8d8
[ 2446.337012] FS:  00007f65d6125c40(0000) GS:ffff888647780000(0000) knlGS:0000000000000000
[ 2446.338579] CS:  0010 DS: 0000 ES: 0000 CR0: 0000000080050033
[ 2446.340134] CR2: 0000000000000000 CR3: 000000050a928004 CR4: 00000000000626e0
[ 2446.341705] Call Trace:
[ 2446.343298]  fill_res_name_pid+0x30/0xa0 [ib_core]
[ 2446.344964]  ? __nla_put+0xc/0x20
[ 2446.346561]  ? fill_res_qp_entry+0x178/0x2a0 [ib_core]
[ 2446.348178]  ? res_get_common_dumpit+0x3a8/0x500 [ib_core]
[ 2446.349785]  ? netlink_dump+0x29a/0x370
[ 2446.351367]  ? __netlink_dump_start+0x1aa/0x220
[ 2446.352975]  ? rdma_nl_rcv_msg+0x149/0x1e0 [ib_core]
[ 2446.354585]  ? nldev_res_get_cm_id_dumpit+0x10/0x10 [ib_core]
[ 2446.356203]  ? rdma_nl_rcv+0xe8/0x140 [ib_core]
[ 2446.357858]  ? netlink_unicast+0x179/0x220
[ 2446.359454]  ? netlink_sendmsg+0x2d2/0x3d0
[ 2446.361004]  ? sock_sendmsg+0x30/0x40
[ 2446.362570]  ? __sys_sendto+0xdc/0x160
[ 2446.364127]  ? sock_alloc_file+0x34/0x90
[ 2446.365656]  ? __sys_socket+0x94/0xe0
[ 2446.367190]  ? __x64_sys_sendto+0x24/0x30
[ 2446.368718]  ? do_syscall_64+0x48/0x130
[ 2446.370281]  ? entry_SYSCALL_64_after_hwframe+0x44/0xa9
[ 2446.371808] Modules linked in: netconsole vhost_net vhost tap vfio_pci vfio_virqfd vfio_iommu_type1 vfio mst_pciconf(OE) nfsv3 nfs_acl nfs lockd grace fscache xt_conntrack nf_conntrack nf_defrag_ipv6 nf_defrag_ipv4 ipt_REJECT nf_reject_ipv4 tun ebtable_filter ebtables ip6table_filter ip6_tables iptable_filter bridge stp llc rdma_ucm(OE) ib_ucm(OE) rdma_cm(OE) iw_cm(OE) ib_ipoib(OE) ib_cm(OE) ib_umad(OE) mlx4_en(OE) mlx4_ib(OE) mlx5_ib(OE) mlx4_core(OE) ib_uverbs(OE) ib_core(OE) sunrpc sb_edac x86_pkg_temp_thermal coretemp kvm_intel kvm mlx5_core(OE) irqbypass mlxfw(OE) mlx_compat(OE) tg3 iTCO_wdt memtrack(OE) ipmi_si pcspkr iTCO_vendor_support crc32_pclmul ptp ipmi_devintf pps_core lpc_ich dcdbas crc32c_intel acpi_power_meter ghash_clmulni_intel ipmi_msghandler wmi mfd_core pcc_cpufreq sch_fq_codel ip_tables mgag200 i2c_algo_bit drm_kms_helper syscopyarea sysfillrect sysimgblt fb_sys_fops ttm sd_mod drm sr_mod megaraid_sas i2c_core cdrom [last unloaded: mst_pci]
[ 2446.386142] CR2: 0000000000000000
[ 2446.388181] ---[ end trace df5f8590cfbc410b ]---
[ 2446.392784] RIP: 0010:strlen+0x0/0x20
[ 2446.393769] Code: f6 82 c0 5b 13 82 20 74 10 48 83 c0 01 0f b6 10 f6 82 c0 5b 13 82 20 75 f0 f3 c3 66 66 66 66 66 66 2e 0f 1f 84 00 00 00 00 00 <80> 3f 00 74 10 48 89 f8 48 83 c0 01 80 38 00 75 f7 48 29 f8 c3 31
[ 2446.395796] RSP: 0018:ffffc9000833f740 EFLAGS: 00010246
[ 2446.396827] RAX: 0000000000000000 RBX: ffff888642f52b00 RCX: 0000000000000000
[ 2446.397839] RDX: 0000000000000001 RSI: ffff888bea2ed8d8 RDI: 0000000000000000
[ 2446.398836] RBP: 0000000000000000 R08: 0000000000000db4 R09: ffff88863f6c9111
[ 2446.399852] R10: 0000000000000000 R11: ffffea002fb84980 R12: ffffc9000833f774
[ 2446.400820] R13: ffff888c3b780000 R14: 0000000000000000 R15: ffff888bea2ed8d8
[ 2446.401785] FS:  00007f65d6125c40(0000) GS:ffff888647780000(0000) knlGS:0000000000000000
[ 2446.402742] CS:  0010 DS: 0000 ES: 0000 CR0: 0000000080050033
[ 2446.403695] CR2: 0000000000000000 CR3: 000000050a928004 CR4: 00000000000626e0
[ 2446.404668] Kernel panic - not syncing: Fatal exception
[ 2446.405701] Kernel Offset: disabled
[ 2446.410229] ---[ end Kernel panic - not syncing: Fatal exception ]---

Issue: 1857969
Fixes: cecbb5018ceb ("IB/uverbs: Add experimental QPs to RDMA resource tracker")

drivers/infiniband/core/uverbs_cmd_exp.c
@@ -323,7 +323,7 @@ int ib_uverbs_exp_create_qp(struct uverbs_attr_bundle *attrs)
 	 */
 	qp->res.type = RDMA_RESTRACK_QP;
 	if (attr->qp_type < IB_QPT_XRC_INI)
-		rdma_restrack_kadd(&qp->res);
+		rdma_restrack_uadd(&qp->res);
 	else
 		rdma_restrack_dontrack(&qp->res);
 