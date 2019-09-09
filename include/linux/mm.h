1ef85c28ac7edd6654c5cc761a36e48950f61f05
Feras Daoud <ferasda@mellanox.com>
2019-09-09 18:22:14 +0300

    Compat: Zero allocated buffer if __GFP_ZERO passed
    Issue: none
    Change-Id: I8b70e0ed6a7a2677170c689e3b3e9944b080af65
    Signed-off-by: Feras Daoud <ferasda@mellanox.com>

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -46,6 +46,10 @@ static inline void *kvmalloc_node(size_t size, gfp_t flags, int node) {
 	rtn = kmalloc_node(size, GFP_KERNEL | __GFP_NOWARN, node);
 	if (!rtn)
 		rtn = vmalloc(size);
+
+	if (rtn && flags & __GFP_ZERO)
+		memset(rtn, 0, size);
+
 	return rtn;
 }
 #endif
