huynguyen85/temp_ipsec_inline_ofed_4_7

commit 078c4e9bfffeeffb6b39a9d2c477982235c3e20b

IB/core: Add src field to the ib_udata struct
src field is added to the ib_udata struct to allow the mlx4
driver to distinguish between EX/EXP/LEGACY provider data

Issue: 865103
Change-Id: I685251d7323d9482dbcfb3ddadf11ff2ea3ce41a
Signed-off-by: Moshe Lazer <moshel@mellanox.com>
Signed-off-by: Maor Gottlieb <maorg@mellanox.com>

drivers/infiniband/core/uverbs.h
@@ -62,6 +62,7 @@ ib_uverbs_init_udata(struct ib_udata *udata,
 	udata->outbuf = obuf;
 	udata->inlen  = ilen;
 	udata->outlen = olen;
+	udata->src = IB_UDATA_LEGACY_CMD;
 }
 
 static inline void
@@ -73,6 +74,7 @@ ib_uverbs_init_udata_buf_or_null(struct ib_udata *udata,
 	ib_uverbs_init_udata(udata,
 			     ilen ? ibuf : NULL, olen ? obuf : NULL,
 			     ilen, olen);
+	udata->src = IB_UDATA_EX_CMD;
 }
 
 /*

drivers/infiniband/core/uverbs_main.c
@@ -759,7 +759,12 @@ static ssize_t ib_uverbs_write(struct file *filp, const char __user *buf,
 			u64_to_user_ptr(ex_hdr.response) + bundle.ucore.outlen,
 			ex_hdr.provider_in_words * 8,
 			ex_hdr.provider_out_words * 8);
-
+/* TALAT TODO _ Yoni should provide a functionality that tell if we are in exp flow 
+		if (exp_cmd) {
+			&bundle.ucore.src = IB_UDATA_EXP_CMD;
+			&bundle.driver_udata.src = IB_UDATA_EXP_CMD;
+		}
+*/
 	}
 
 	ret = method_elm->handler(&bundle);

include/rdma/ib_verbs.h
@@ -1591,11 +1591,18 @@ struct ib_uobject {
 	const struct uverbs_api_object *uapi_object;
 };
 
+enum ib_udate_src {
+	IB_UDATA_LEGACY_CMD,
+	IB_UDATA_EX_CMD,
+	IB_UDATA_EXP_CMD = 32,
+};
+
 struct ib_udata {
 	const void __user *inbuf;
 	void __user *outbuf;
 	size_t       inlen;
 	size_t       outlen;
+	enum ib_udate_src	src;
 };
 
 struct ib_pd {

commit 90426bc4bdf447409208cc0f3090516fdde3e4f2

IB/uverbs: Initialize experimental MD for user data
Due to the rebase process there are some upstream flows which don't
initialize experimental MD of the udata buffer.

This is an opportunity to replace the historical and almost unused 'enum
ib_udate_src' enumeration by just a flag and remove some unused
declarations.

Fixes: 078c4e9bfffe ("IB/core: Add src field to the ib_udata struct")
Issue: 1909908
Change-Id: I1a0d643b512f8b733ac90c999ac84c84549504c6
Signed-off-by: Guy Levi <guyle@mellanox.com>

drivers/infiniband/core/uverbs.h
@@ -84,7 +84,7 @@ ib_uverbs_init_udata(struct ib_udata *udata,
 	udata->outbuf = obuf;
 	udata->inlen  = ilen;
 	udata->outlen = olen;
-	udata->src = IB_UDATA_LEGACY_CMD;
+	udata->is_exp = 0;
 }
 
 static inline void
@@ -96,7 +96,6 @@ ib_uverbs_init_udata_buf_or_null(struct ib_udata *udata,
 	ib_uverbs_init_udata(udata,
 			     ilen ? ibuf : NULL, olen ? obuf : NULL,
 			     ilen, olen);
-	udata->src = IB_UDATA_EX_CMD;
 }
 
 /*

drivers/infiniband/core/uverbs_cmd.c
@@ -3074,7 +3074,7 @@ static int ib_uverbs_ex_modify_wq(struct uverbs_attr_bundle *attrs)
 	/* exp cmd can be without attr_mask, some values are passed via vendor
 	 * channel.
 	 */
-	if ((!cmd.attr_mask) && (attrs->ucore.src != IB_UDATA_EXP_CMD))
+	if ((!cmd.attr_mask) && !attrs->ucore.is_exp)
 		return -EINVAL;
 
 	if (cmd.attr_mask > (IB_WQ_STATE | IB_WQ_CUR_STATE | IB_WQ_FLAGS))
@@ -3520,7 +3520,7 @@ static int __uverbs_create_xsrq(struct uverbs_attr_bundle *attrs,
 	if (cmd->srq_type == IB_SRQT_XRC)
 		resp.srqn = srq->ext.xrc.srq_num;
 
-	if (udata->src == IB_UDATA_EXP_CMD) {
+	if (udata->is_exp) {
 		ret = ib_uverbs_exp_create_srq_resp(&resp, cmd->response);
 	} else {
 		if (copy_to_user(u64_to_user_ptr(cmd->response),

drivers/infiniband/core/uverbs_ioctl.c
@@ -761,6 +761,10 @@ void uverbs_fill_udata(struct uverbs_attr_bundle *bundle,
 		udata->outbuf = NULL;
 		udata->outlen = 0;
 	}
+
+	/* There aren't experimental verbs over new uverbs infrastructure */
+	udata->is_exp = 0;
+
 }
 
 int uverbs_copy_to(const struct uverbs_attr_bundle *bundle, size_t idx,

drivers/infiniband/core/uverbs_main.c
@@ -750,10 +750,6 @@ static ssize_t ib_uverbs_exp_write(struct file *filp,
 	struct uverbs_attr_bundle bundle;
 	int srcu_key;
 	u32 command;
-
-	struct ib_udata ucore;
-	struct ib_udata uhw;
-
 	ssize_t ret;
 
 	ret = process_exp_hdr(hdr, &command);
@@ -795,8 +791,8 @@ static ssize_t ib_uverbs_exp_write(struct file *filp,
 					 ex_hdr.provider_in_words * 8,
 					 ex_hdr.provider_out_words * 8);
 
-	bundle.ucore.src = IB_UDATA_EXP_CMD;
-	bundle.driver_udata.src = IB_UDATA_EXP_CMD;
+	bundle.ucore.is_exp = 1;
+	bundle.driver_udata.is_exp = 1;
 
 	ret = uverbs_exp_cmd_table[command](&bundle);
 	ret = (ret) ? : count;
@@ -868,6 +864,8 @@ static ssize_t ib_uverbs_write(struct file *filp, const char __user *buf,
 				bundle.driver_udata.inbuf = buf + in_len;
 			else
 				bundle.driver_udata.inbuf = NULL;
+
+			bundle.driver_udata.is_exp = 0;
 		} else {
 			memset(&bundle.driver_udata, 0,
 			       sizeof(bundle.driver_udata));

drivers/infiniband/hw/mlx5/cq.c
@@ -729,7 +729,7 @@ static int create_cq_user(struct mlx5_ib_dev *dev, struct ib_udata *udata,
 
 	memset(&ucmd, 0, sizeof(ucmd));
 
-	if (udata->src == IB_UDATA_EXP_CMD)
+	if (udata->is_exp)
 		ucmdlen = min(sizeof(ucmd), udata->inlen);
 	else
 		ucmdlen = udata->inlen < sizeof(struct mlx5_ib_create_cq) ?

drivers/infiniband/hw/mlx5/qp.c
@@ -1213,7 +1213,7 @@ static int create_user_qp(struct mlx5_ib_dev *dev, struct ib_pd *pd,
 		goto err_free;
 	}
 
-	if (udata->src == IB_UDATA_EXP_CMD) {
+	if (udata->is_exp) {
 		set_exp_qp_resp_data(ucmd, &resp, dev, qp, attr);
 		err = ib_copy_to_udata(udata, &resp, min(udata->outlen, sizeof(resp)));
 	} else {
@@ -4515,7 +4515,7 @@ int mlx5_ib_modify_qp(struct ib_qp *ibqp, struct ib_qp_attr *attr,
 	size_t required_cmd_sz;
 	int err = -EINVAL;
 	int port;
-	bool is_exp = udata && (udata->src == IB_UDATA_EXP_CMD);
+	bool is_exp = udata && udata->is_exp;
 
 	if (ibqp->rwq_ind_tbl)
 		return -ENOSYS;
@@ -6765,7 +6765,7 @@ static int prepare_user_rq(struct ib_pd *pd,
 	int err;
 	size_t required_cmd_sz;
 
-	if (udata->src == IB_UDATA_EXP_CMD) {
+	if (udata->is_exp) {
 		err = mlx5_ib_exp_get_cmd_data(dev, udata, &cmd_data);
 		if (err)
 			return err;
@@ -7025,7 +7025,7 @@ int mlx5_ib_modify_wq(struct ib_wq *wq, struct ib_wq_attr *wq_attr,
 	void *rqc;
 	void *in;
 
-	if (udata->src == IB_UDATA_EXP_CMD) {
+	if (udata->is_exp) {
 		if (ib_copy_from_udata(&eucmd, udata, min(sizeof(eucmd), udata->inlen)))
 			return -EFAULT;
 		goto common;
@@ -7066,7 +7066,7 @@ common:
 	MLX5_SET(modify_rq_in, in, uid, to_mpd(wq->pd)->uid);
 	MLX5_SET(rqc, rqc, state, wq_state);
 
-	if (udata->src == IB_UDATA_EXP_CMD) {
+	if (udata->is_exp) {
 		if (eucmd.attr_mask & MLX5_EXP_MODIFY_WQ_VLAN_OFFLOADS) {
 			if (!(MLX5_CAP_GEN(dev->mdev, eth_net_offloads) &&
 			      MLX5_CAP_ETH(dev->mdev, vlan_cap))) {

drivers/infiniband/hw/mlx5/srq.c
@@ -57,7 +57,7 @@ static int create_srq_user(struct ib_pd *pd, struct mlx5_ib_srq *srq,
 	u32 offset;
 	u32 uidx = MLX5_IB_DEFAULT_UIDX;
 
-	if (udata->src == IB_UDATA_EXP_CMD) {
+	if (udata->is_exp) {
 		err = mlx5_ib_exp_create_srq_user(dev, in, udata, &ucmd);
 		if (err)
 			return err;

include/rdma/ib_verbs.h
@@ -1535,12 +1535,6 @@ struct ib_uobject {
 	const struct uverbs_api_object *uapi_object;
 };
 
-enum ib_udate_src {
-	IB_UDATA_LEGACY_CMD,
-	IB_UDATA_EX_CMD,
-	IB_UDATA_EXP_CMD = 32,
-};
-
 struct ib_udata;
 struct ib_udata_ops {
 	int     (*copy_from)(void *dest, struct ib_udata *udata,
@@ -1555,7 +1549,7 @@ struct ib_udata {
 	void __user *outbuf;
 	size_t       inlen;
 	size_t       outlen;
-	enum ib_udate_src	src;
+	bool is_exp;
 };
 
 struct ib_pd {