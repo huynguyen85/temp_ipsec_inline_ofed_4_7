40073a5c959b78138b90686d2dc473340e3028bd
Talat Batheesh <talatb@mellanox.com>
2019-10-14 20:37:56 -0500

    BACKPORT: include/linux/mlx4/cq_exp.h
    Change-Id: I07e4ebd67fb614ab850ca3a97336a9c1ffc9aeb4

--- include/linux/mlx4/cq_exp.h
+++ include/linux/mlx4/cq_exp.h
@@ -2,7 +2,9 @@
 #define MLX4_CQ_EXP_H
 
 #include <linux/types.h>
+#ifdef HAVE_UAPI_LINUX_IF_ETHER_H
 #include <uapi/linux/if_ether.h>
+#endif
 
 #include <linux/mlx4/device.h>
 #include <linux/mlx4/doorbell.h>
