a0532b48364cfc0575113b86c142cbd005d9aae9
Talat Batheesh <talatb@mellanox.com>
2019-10-14 20:37:55 -0500

    BACKPORT: drivers/net/ethernet/mellanox/mlx4/icm.h
    Change-Id: I349b23f5d90bb62e864c8561ccb6c6c4feab4c07

--- drivers/net/ethernet/mellanox/mlx4/icm.h
+++ drivers/net/ethernet/mellanox/mlx4/icm.h
@@ -81,7 +81,12 @@ struct mlx4_icm *mlx4_alloc_icm(struct mlx4_dev *dev, int npages,
 				gfp_t gfp_mask, int coherent);
 void mlx4_free_icm(struct mlx4_dev *dev, struct mlx4_icm *icm, int coherent);
 
+#ifdef HAVE_MEMALLOC_NOIO_SAVE
 int mlx4_table_get(struct mlx4_dev *dev, struct mlx4_icm_table *table, u32 obj);
+#else
+int mlx4_table_get(struct mlx4_dev *dev, struct mlx4_icm_table *table, u32 obj,
+		   gfp_t gfp);
+#endif
 void mlx4_table_put(struct mlx4_dev *dev, struct mlx4_icm_table *table, u32 obj);
 int mlx4_table_get_range(struct mlx4_dev *dev, struct mlx4_icm_table *table,
 			 u32 start, u32 end);
